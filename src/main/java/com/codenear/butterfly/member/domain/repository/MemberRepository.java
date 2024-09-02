8e96ea1816c7ca54f4394b575fbf938bd7ab2dc5
taek2222 <oot309@daum.net>
2024-09-03 00:12:46 +0900

    feat: FE 요청 데이터 기반 회원가입/로그인 추가 (#19)

--- src/main/java/com/codenear/butterfly/member/domain/repository/MemberRepository.java
+++ src/main/java/com/codenear/butterfly/member/domain/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.codenear.butterfly.member.domain.repository;
 
 import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.member.domain.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmailAndPlatform(String email, Platform platform);
 }
