ce0560acb85bdf1c5832321c7b2da4e1f1796a4a
taek2222 <oot309@daum.net>
2024-09-03 00:11:13 +0900

    feat: FE 요청 데이터 기반 회원가입/로그인 추가 (#19)

---
+++ src/main/java/com/codenear/butterfly/auth/application/AuthService.java
@@ -0,0 +1,38 @@
+package com.codenear.butterfly.auth.application;
+
+import com.codenear.butterfly.member.domain.Grade;
+import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.member.domain.repository.MemberRepository;
+import com.codenear.butterfly.auth.domain.dto.AuthRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+
+    public void registerOrLogin(AuthRequestDTO requestDTO) {
+        Optional<Member> member = memberRepository.findByEmailAndPlatform(requestDTO.getEmail(), requestDTO.getPlatform());
+
+        if (member.isEmpty()) { // 회원 정보 DB 저장
+            Member registerMember = register(requestDTO);
+            memberRepository.save(registerMember);
+        }
+    }
+
+    private Member register(AuthRequestDTO requestDTO) {
+        return Member.builder()
+                .email(requestDTO.getEmail())
+                .nickname(requestDTO.getNickname())
+                .point(0)
+                .grade(Grade.LEVEL_1)
+                .platform(requestDTO.getPlatform())
+                .build();
+    }
+}
