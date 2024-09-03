36d672b26605802f20adb696738538f71911f69f
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-04 00:19:12 +0900

    refactor: 보안 강화를 위한 로직 변경

--- src/main/java/com/codenear/butterfly/auth/application/CustomUserDetailsService.java
+++ src/main/java/com/codenear/butterfly/auth/application/CustomUserDetailsService.java
@@ -2,12 +2,15 @@ package com.codenear.butterfly.auth.application;
 
 import com.codenear.butterfly.auth.domain.dto.CustomUserDetails;
 import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.member.domain.Role;
 import com.codenear.butterfly.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,11 @@ public class CustomUserDetailsService implements UserDetailsService {
                         errorMessageService.getErrorMessage("error.userNotFound", email)
                 ));
 
-        return new CustomUserDetails(member);
+        return new CustomUserDetails(
+                member.getId(),
+                member.getEmail(),
+                member.getPassword(),
+                member.getRoles().stream().map(Role::name).collect(Collectors.toSet())
+        );
     }
 }
