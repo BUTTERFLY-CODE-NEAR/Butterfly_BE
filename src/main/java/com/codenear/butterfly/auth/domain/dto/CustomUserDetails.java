7e98a50941545aab72ea84e7f4aae63dc47f8bac
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-04 00:19:35 +0900

    refactor: 보안 강화를 위한 로직 변경

--- src/main/java/com/codenear/butterfly/auth/domain/dto/CustomUserDetails.java
+++ src/main/java/com/codenear/butterfly/auth/domain/dto/CustomUserDetails.java
@@ -1,34 +1,40 @@
 package com.codenear.butterfly.auth.domain.dto;
 
 import com.codenear.butterfly.member.domain.Member;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+@Getter
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
-    private final Member member;
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final Set<String> roles;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return member.getRoles().stream()
+        return roles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                 .collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        return member.getPassword();
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return member.getEmail();
+        return this.email;
     }
 
     @Override
