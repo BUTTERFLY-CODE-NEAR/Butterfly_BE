dcb0d9a5a56c5e9d03b91239afb06e8356637b49
taek2222 <oot309@daum.net>
2024-09-05 16:18:02 +0900

    refactor: 충돌로 인한 이름 변경 (#11)

--- src/main/java/com/codenear/butterfly/auth/jwt/JwtUtil.java
+++ src/main/java/com/codenear/butterfly/auth/jwt/JwtUtil.java
@@ -1,6 +1,6 @@
 package com.codenear.butterfly.auth.jwt;
 
-import com.codenear.butterfly.auth.domain.dto.OAuthRequestDTO;
+import com.codenear.butterfly.auth.domain.dto.AuthRequestDTO;
 import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -63,7 +63,7 @@ public class JwtUtil {
                 .before(new Date());
     }
 
-    public String createJwt(OAuthRequestDTO authRequestDTO) {
+    public String createJwt(AuthRequestDTO authRequestDTO) {
         return Jwts.builder()
                 .claim("mail", authRequestDTO.getEmail())
                 .claim("nickname", authRequestDTO.getNickname())
--- src/main/java/com/codenear/butterfly/auth/presentation/AuthController.java
+++ src/main/java/com/codenear/butterfly/auth/presentation/AuthController.java
@@ -1,22 +1,25 @@
 package com.codenear.butterfly.auth.presentation;
 
-import com.codenear.butterfly.auth.domain.dto.AuthRequestDTO;
 import com.codenear.butterfly.auth.application.AuthService;
+import com.codenear.butterfly.auth.domain.dto.AuthRequestDTO;
+import com.codenear.butterfly.auth.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@ResponseBody
 public class AuthController {
-
+    private final JwtUtil jwtUtil;
     private final AuthService authService;
 
-    @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody AuthRequestDTO authRequestDTO) {
+    @PostMapping("/auth/login")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void login(@RequestBody AuthRequestDTO authRequestDTO, HttpServletResponse response) {
         authService.registerOrLogin(authRequestDTO);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        String token = jwtUtil.createJwt(authRequestDTO);
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
