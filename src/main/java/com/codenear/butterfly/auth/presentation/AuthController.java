0f2fedabd3fb35dff4c52bcbb29651650e77c5dd
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-07 07:14:46 +0900

    refactor: jwtService의 새로운 로직을 이용하여 코드 개선

--- src/main/java/com/codenear/butterfly/auth/presentation/AuthController.java
+++ src/main/java/com/codenear/butterfly/auth/presentation/AuthController.java
@@ -2,16 +2,20 @@ package com.codenear.butterfly.auth.presentation;
 
 import com.codenear.butterfly.auth.application.AuthService;
 import com.codenear.butterfly.auth.application.MessageService;
+import com.codenear.butterfly.auth.application.email.EmailLoginService;
 import com.codenear.butterfly.auth.application.email.EmailRegisterService;
 import com.codenear.butterfly.auth.application.jwt.JwtService;
 import com.codenear.butterfly.auth.domain.dto.AuthRequestDTO;
+import com.codenear.butterfly.auth.domain.dto.CustomUserDetails;
 import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.member.domain.Platform;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,23 +30,59 @@ public class AuthController {
     private final JwtService jwtService;
     private final EmailRegisterService emailRegisterService;
     private final MessageService messageService;
+    private final EmailLoginService emailLoginService;
 
     @PostMapping("/auth/register")
     public ResponseEntity<String> register(@RequestBody AuthRequestDTO authRequestDTO) {
+        log.info(messageService.getMessage("log.registerRequest", authRequestDTO.getEmail()));
+
         try {
             Member member = emailRegisterService.emailRegister(authRequestDTO);
-            log.info(messageService.getMessage("success.register", authRequestDTO.getEmail()));
-            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 성공적으로 완료되었습니다. 사용자 ID: " + member.getId());
+            log.info(messageService.getMessage("log.registerSuccess", member.getId()));
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(messageService.getMessage("success.register", member.getEmail()));
         } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageService.getMessage("error.register"));
+            log.error(messageService.getMessage("log.registerFailure", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(messageService.getMessage("error.register"));
         }
     }
 
     @PostMapping("/auth/login")
     @ResponseStatus(HttpStatus.OK)
-    public void login(@RequestBody AuthRequestDTO requestDTO, HttpServletResponse response) {
-        authService.registerOrLogin(requestDTO);
-        jwtService.processTokens(requestDTO.getEmail(), requestDTO.getPlatform().name(), response);
+    public ResponseEntity<?> login(@RequestBody AuthRequestDTO requestDTO, HttpServletResponse response) {
+        log.info(messageService.getMessage("log.loginRequest", requestDTO.getEmail(), requestDTO.getPlatform()));
+
+        try {
+            if (requestDTO.getPlatform() == Platform.CODENEAR) {
+                log.info(messageService.getMessage("log.loginEmailAttempt", requestDTO.getEmail()));
+
+                CustomUserDetails loginUser = emailLoginService.login(requestDTO.getEmail(), requestDTO.getPassword());
+                jwtService.processTokens(requestDTO.getEmail(), requestDTO.getPlatform().name(), response);
+                log.info(messageService.getMessage("log.jwtCreated", loginUser.getEmail()));
+                log.info(messageService.getMessage("log.loginEmailSuccess", loginUser.getEmail()));
+
+                return ResponseEntity.ok(messageService.getMessage("log.loginEmailSuccess", loginUser.getEmail()));
+            } else {
+                log.info(messageService.getMessage("log.loginSocialAttempt", requestDTO.getEmail()));
+
+                authService.registerOrLogin(requestDTO);
+                jwtService.processTokens(requestDTO.getEmail(), requestDTO.getPlatform().name(), response);
+                log.info(messageService.getMessage("log.jwtCreated", requestDTO.getEmail()));
+                log.info(messageService.getMessage("log.loginSocialSuccess", requestDTO.getEmail()));
+
+                return ResponseEntity.ok(messageService.getMessage("log.loginSocialSuccess", requestDTO.getEmail()));
+            }
+        } catch (BadCredentialsException e) {
+            log.error(messageService.getMessage("error.badCredentials", requestDTO.getEmail()));
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(messageService.getMessage("error.badCredentials", requestDTO.getEmail()));
+        } catch (Exception e) {
+            log.error(messageService.getMessage("error.internalServerError", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(messageService.getMessage("error.internalServerError", e.getMessage()));
+        }
     }
 
     @PostMapping("/reissue")
