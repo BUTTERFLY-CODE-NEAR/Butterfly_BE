f5305cd56dfb2d0953dfa8195ebca9ba454f6ccd
taek2222 <oot309@daum.net>
2024-09-06 20:38:46 +0900

    refactor: 리프레쉬 토큰 추가로 인한 요소 변경 (#33)

--- src/main/java/com/codenear/butterfly/auth/jwt/JwtUtil.java
+++ src/main/java/com/codenear/butterfly/auth/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.codenear.butterfly.auth.jwt;
 
-import com.codenear.butterfly.auth.domain.dto.AuthRequestDTO;
+import com.codenear.butterfly.auth.domain.JwtRefresh;
+import com.codenear.butterfly.member.domain.Member;
 import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,12 +21,12 @@ public class JwtUtil {
     @Value("${jwt.access-token-expiration-millis}")
     private long accessTokenExpirationMillis;
 
-//    @Value("${jwt.refresh-token-expiration-millis}")
-//    private long refreshTokenExpirationMillis;
+    @Value("${jwt.refresh-token-expiration-millis}")
+    private long refreshTokenExpirationMillis;
 
     private SecretKey secretKey;
 
-    @PostConstruct
+    @PostConstruct // 스프링 빈 초기화 이후 실행되는 로직
     public void init() {
         this.secretKey = new SecretKeySpec(
                 key.getBytes(StandardCharsets.UTF_8),
@@ -35,22 +36,39 @@ public class JwtUtil {
                     .getAlgorithm());
     }
 
-    public String getEmail(String token) {
-        return Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .get("mail", String.class);
+    public String createAccessJwt(Long memberId) {
+        return createJwt("Access", memberId, accessTokenExpirationMillis);
     }
 
-    public String getPlatform(String token) {
+    public String createRefreshJwt(Long memberId) {
+        return createJwt("Refresh", memberId, refreshTokenExpirationMillis);
+    }
+
+    private String createJwt(String category, Long memberId, Long tokenExpirationMillis) {
+        return Jwts.builder()
+                .claim("category", category)
+                .claim("memberId", String.valueOf(memberId))
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + tokenExpirationMillis))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String getCategory(String token) {
+        return getMemberInfo(token, "category");
+    }
+
+    public Long getMemberId(String token) {
+        return Long.parseLong(getMemberInfo(token, "memberId"));
+    }
+
+    private String getMemberInfo(String token, String info) {
         return Jwts.parser()
                 .verifyWith(secretKey)
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
-                .get("platform", String.class);
+                .get(info, String.class);
     }
 
     public Boolean isExpired(String token) {
@@ -63,14 +81,11 @@ public class JwtUtil {
                 .before(new Date());
     }
 
-    public String createJwt(AuthRequestDTO authRequestDTO) {
-        return Jwts.builder()
-                .claim("mail", authRequestDTO.getEmail())
-                .claim("nickname", authRequestDTO.getNickname())
-                .claim("platform", authRequestDTO.getPlatform().name())
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + accessTokenExpirationMillis))
-                .signWith(secretKey)
-                .compact();
+    public JwtRefresh buildRefreshEntity(Member member, String refresh) {
+        return JwtRefresh.builder()
+                .member(member)
+                .refresh(refresh)
+                .expiration(new Date(System.currentTimeMillis() + refreshTokenExpirationMillis))
+                .build();
     }
 }
