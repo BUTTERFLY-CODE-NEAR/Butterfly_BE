0eb7452f9b6a81498ef1328427a70ddeec30fd86
taek2222 <oot309@daum.net>
2024-09-07 15:53:27 +0900

    feat: Swagger API 명세서 작성 (#43)

--- src/main/java/com/codenear/butterfly/auth/domain/dto/AuthRequestDTO.java
+++ src/main/java/com/codenear/butterfly/auth/domain/dto/AuthRequestDTO.java
@@ -1,12 +1,25 @@
 package com.codenear.butterfly.auth.domain.dto;
 
 import com.codenear.butterfly.member.domain.Platform;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
+@Schema(title = "회원가입/로그인 API JSON", description = "회원가입 및 로그인할 때 정의되는 JSON 데이터 입니다.")
 public class AuthRequestDTO {
+
+    @NotNull
+    @Schema(description = "회원가입 및 로그인 이메일", example = "codenear@naver.com")
     private String email;
+
+    @Schema(description = "회원가입 및 로그인 비밀번호 (GOOGLE, KAKAO 플랫폼에는 미사용)", example = "password")
     private String password;
+
+    @Schema(description = "닉네임 (CODE NEAR 플랫폼 회원가입 전달 시, 필수 데이터", example = "codenear")
     private String nickname;
+
+    @NotNull
+    @Schema(description = "플랫폼 종류", examples = {"GOOGLE", "KAKAO", "CODENEAR"})
     private Platform platform;
 }
