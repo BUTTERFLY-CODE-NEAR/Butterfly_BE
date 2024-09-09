bb050b1c868b2227299105df765800c167279160
taek2222 <oot309@daum.net>
2024-09-09 15:24:39 +0900

    refactor: Platform JSON <-> JAVA 변환 지정 (#61)

--- src/main/java/com/codenear/butterfly/member/domain/Platform.java
+++ src/main/java/com/codenear/butterfly/member/domain/Platform.java
@@ -1,7 +1,24 @@
 package com.codenear.butterfly.member.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Platform {
     KAKAO,
     GOOGLE,
-    CODENEAR
+    CODENEAR;
+
+    @JsonCreator // Json -> Java
+    public static Platform fromString(String key) {
+        try {
+            return Platform.valueOf(key.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    @JsonValue // Java -> Json
+    public String toValue() {
+        return this.name();
+    }
 }
