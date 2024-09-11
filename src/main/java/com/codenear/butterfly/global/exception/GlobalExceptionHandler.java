abcea7dccff804e5d9c398f98caa606b427fc635
taek2222 <oot309@daum.net>
2024-09-11 14:08:12 +0900

    refactor: 클래스 이름 변경 및 새로운 응답 객체에 알맞게 변경 (#67)

---
+++ src/main/java/com/codenear/butterfly/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.codenear.butterfly.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codenear.butterfly.global.util.ResponseUtil.createErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return createErrorResponse(ErrorCode.VALIDATION_FAILED, errors);
+    }
+}
