ace7baabcab4de44fa415281a4f54c01144c8073
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-12 22:57:23 +0900

    feat: 닉네임 swagger 인터페이스 추가

---
+++ src/main/java/com/codenear/butterfly/member/presentation/NicknameApiSwagger.java
@@ -0,0 +1,14 @@
+package com.codenear.butterfly.member.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Tag(name = "Member", description = "닉네임 생성 Api")
+public interface NicknameApiSwagger {
+
+    @Operation(summary = "닉네임 생성", description = "랜덤 닉네임 생성 API(닉네임 중복 시 숫자 태그 추가")
+    @RequestMapping(value = "/nickname/generate", method = RequestMethod.GET)
+    String nicknameGenerate();
+}
