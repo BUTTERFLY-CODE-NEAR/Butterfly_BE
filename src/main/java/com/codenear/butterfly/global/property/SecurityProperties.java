c087e15db245c3501ca0b6497749ebfd8f82cdad
taek2222 <oot309@daum.net>
2024-09-13 00:03:29 +0900

    feat: URL 기반 화이트 리스트 체크 메서드 추가 (#74)

--- src/main/java/com/codenear/butterfly/global/property/SecurityProperties.java
+++ src/main/java/com/codenear/butterfly/global/property/SecurityProperties.java
@@ -3,6 +3,7 @@ package com.codenear.butterfly.global.property;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.Arrays;
 
@@ -10,6 +11,8 @@ import java.util.Arrays;
 public class SecurityProperties {
     @Value("${SECURITY_WHITELIST:}")
     private String whitelist;
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
     public String[] getWhitelistArray() {
         if (whitelist == null) {
@@ -19,5 +22,10 @@ public class SecurityProperties {
         return Arrays.stream(whitelist.split(","))
                 .filter(StringUtils::hasText)
                 .toArray(String[]::new);
+    }
+
+    public boolean isWhitelisted(String url) {
+        return Arrays.stream(getWhitelistArray())
+                .anyMatch(pattern -> pathMatcher.match(pattern, url));
     }
 }
