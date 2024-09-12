b6e5284611cb4f7518b39128b979445e4b917cfc
taek2222 <oot309@daum.net>
2024-09-13 00:01:25 +0900

    refactor: JWT 에러 코드 수정 및 전체적인 리팩토링 진행 (#74)

--- src/main/java/com/codenear/butterfly/auth/presentation/JwtFilter.java
+++ src/main/java/com/codenear/butterfly/auth/presentation/JwtFilter.java
@@ -1,10 +1,14 @@
 package com.codenear.butterfly.auth.presentation;
 
 import com.codenear.butterfly.auth.jwt.JwtUtil;
+import com.codenear.butterfly.global.dto.ResponseDTO;
+import com.codenear.butterfly.global.exception.ErrorCode;
+import com.codenear.butterfly.global.property.SecurityProperties;
 import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.member.domain.Platform;
-import com.codenear.butterfly.member.domain.repository.MemberRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,63 +24,93 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String CONTENT_TYPE_JSON = "application/json";
+    private static final String CHARSET_UTF_8 = "UTF-8";
+
     private final JwtUtil jwtUtil;
-    private final MemberRepository memberRepository;
+    private final SecurityProperties securityProperties;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String accessToken = request.getHeader("Authorization");
+        String requestURL = request.getRequestURI();
 
-        if (accessToken == null) {
+        if (securityProperties.isWhitelisted(requestURL)) {
             filterChain.doFilter(request, response);
             return;
         }
 
+        String authorization = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (isTokenNull(response, authorization)) return;
+
+        String token = authorization.split(" ")[1];
+
+        if (isTokenExpired(response, token)) return;
+
+        String email = jwtUtil.getEmail(token);
+        String platform = jwtUtil.getPlatform(token);
+
+        Member member = Member.builder()
+                .email(email)
+                .platform(Platform.valueOf(platform))
+                .build();
+
+        setAuthentication(member);
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isTokenNull(HttpServletResponse response, String accessToken) throws IOException {
+        if (accessToken == null || !accessToken.startsWith("Bearer ")) {
+            writeResponse(response, ErrorCode.NULL_JWT_TOKEN);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isTokenExpired(HttpServletResponse response, String accessToken) throws IOException {
         try {
             jwtUtil.isExpired(accessToken);
         } catch (ExpiredJwtException e) {
-            PrintWriter writer = response.getWriter();
-            writer.print("토큰이 만료 되었습니다.");
-
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+            writeResponse(response, ErrorCode.EXPIRED_JWT_TOKEN);
+            return true;
+        } catch (SignatureException e) {
+            writeResponse(response, ErrorCode.INVALID_JWT_SIGNATURE);
+            return true;
         }
+        return false;
+    }
 
-        String category = jwtUtil.getCategory(accessToken);
-
-        if (!category.equals("Access")) {
-            PrintWriter writer = response.getWriter();
-            writer.print("유효하지 않은 토큰 입니다.");
-
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
-        }
-
-        String email = jwtUtil.getEmail(accessToken);
-        String platform = jwtUtil.getPlatform(accessToken);
-
-        Optional<Member> member = memberRepository.findByEmailAndPlatform(email, Platform.valueOf(platform));
-
-        if (member.isEmpty()) {
-            PrintWriter writer = response.getWriter();
-            writer.print("알맞은 회원 정보가 없습니다.");
-
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
+    // 인증 정보 저장
+    private static void setAuthentication(Member member) {
         Authentication authToken = new UsernamePasswordAuthenticationToken(
-                member.get(),
+                member,
                 null,
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
 
         SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
 
-        filterChain.doFilter(request, response);
+    // Filter 에러 응답 생성
+    private void writeResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setStatus(errorCode.getStatus().value());
+        response.setContentType(CONTENT_TYPE_JSON);
+        response.setCharacterEncoding(CHARSET_UTF_8);
+
+        ResponseDTO responseDTO = new ResponseDTO(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                null);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(responseDTO);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(jsonResponse);
+        writer.flush();
     }
 }
