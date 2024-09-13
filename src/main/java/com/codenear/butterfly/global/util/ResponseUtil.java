cf7f20ac377f4b9abd80c9fe1431ddc7d66ce978
taek2222 <oot309@daum.net>
2024-09-13 19:26:43 +0900

    refactor: 반환 타입을 ? 대신, 반환 DTO 변경 (#79)

--- src/main/java/com/codenear/butterfly/global/util/ResponseUtil.java
+++ src/main/java/com/codenear/butterfly/global/util/ResponseUtil.java
@@ -10,21 +10,21 @@ public class ResponseUtil {
     private static final String DEFAULT_SUCCESS_MESSAGE = "성공적으로 처리 완료 되었습니다.";
     private static final HttpStatus DEFAULT_SUCCESS_STATUS = HttpStatus.OK;
 
-    public static ResponseEntity<?> createSuccessResponse(Object body) {
+    public static ResponseEntity<ResponseDTO> createSuccessResponse(Object body) {
         return createSuccessResponse(DEFAULT_SUCCESS_STATUS , DEFAULT_SUCCESS_MESSAGE, body);
     }
 
-    public static ResponseEntity<?> createSuccessResponse(String message, Object body) {
+    public static ResponseEntity<ResponseDTO> createSuccessResponse(String message, Object body) {
         return createSuccessResponse(DEFAULT_SUCCESS_STATUS , message, body);
     }
 
-    public static ResponseEntity<?> createSuccessResponse(HttpStatus status, String message, Object body) {
+    public static ResponseEntity<ResponseDTO> createSuccessResponse(HttpStatus status, String message, Object body) {
         return ResponseEntity
                 .status(status)
                 .body(new ResponseDTO(status.value(), message, body));
     }
 
-    public static ResponseEntity<?> createErrorResponse(ErrorCode errorCode, Object body) {
+    public static ResponseEntity<ResponseDTO> createErrorResponse(ErrorCode errorCode, Object body) {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(new ResponseDTO(
