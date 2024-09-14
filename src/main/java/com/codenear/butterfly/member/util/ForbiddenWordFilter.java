5bded68cdbc15418995ab39405c5697b11f12504
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-14 21:53:42 +0900

    refactor: 트라이 자료구조 미사용

--- src/main/java/com/codenear/butterfly/member/util/ForbiddenWordFilter.java
+++ src/main/java/com/codenear/butterfly/member/util/ForbiddenWordFilter.java
@@ -3,34 +3,13 @@ package com.codenear.butterfly.member.util;
 import com.codenear.butterfly.member.domain.ForbiddenWord;
 import com.codenear.butterfly.member.domain.repository.nickname.ForbiddenWordRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ForbiddenWordFilter {
 
     private final ForbiddenWordRepository forbiddenWordRepository;
-    private TrieNode root;
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void init() {
-        root = new TrieNode();
-        List<ForbiddenWord> forbiddenWords = forbiddenWordRepository.findAll();
-        System.out.println("Forbidden words from DB: " + forbiddenWords);
-        forbiddenWords.forEach(word -> insert(word.getWord()));
-    }
-
-    private void insert(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            node = node.getChildren().computeIfAbsent(c, k -> new TrieNode());
-        }
-        node.setEndOfWord(true);
-    }
 
     public boolean containsForbiddenWord(String text) {
         String filteredText = text.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣]", "");
