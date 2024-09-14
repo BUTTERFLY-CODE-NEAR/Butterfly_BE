170e0fd4135413fd8ce2150055e354748f635de8
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-14 21:55:24 +0900

    feat: 닉네임 비속어 필터링 적용

--- src/main/java/com/codenear/butterfly/auth/application/email/EmailRegisterService.java
+++ src/main/java/com/codenear/butterfly/auth/application/email/EmailRegisterService.java
@@ -6,6 +6,7 @@ import com.codenear.butterfly.global.exception.ErrorCode;
 import com.codenear.butterfly.member.domain.Grade;
 import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.member.domain.repository.member.MemberRepository;
+import com.codenear.butterfly.member.util.ForbiddenWordFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,14 +21,23 @@ public class EmailRegisterService {
     private final CustomUserDetailsService userDetailsService;
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ForbiddenWordFilter forbiddenWordFilter;
 
     public void emailRegister(AuthRequestDTO authRequestDTO) {
         if (hasMember(authRequestDTO.getEmail())) {
             throw new AuthException(ErrorCode.EMAIL_ALREADY_IN_USE, authRequestDTO.getEmail());
         }
 
+        validateNickname(authRequestDTO.getNickname());
+
         Member newMember = register(authRequestDTO);
         memberRepository.save(newMember);
+    }
+
+    private void validateNickname(String nickname) {
+        if (forbiddenWordFilter.containsForbiddenWord(nickname)) {
+            throw new AuthException(ErrorCode.FORBIDDEN_NICKNAME, nickname);
+        }
     }
 
     private boolean hasMember(String email) {
