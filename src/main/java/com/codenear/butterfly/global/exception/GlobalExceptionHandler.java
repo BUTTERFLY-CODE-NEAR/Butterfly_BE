747373c4e5ff102f3e77e639f82d6d9803ce2acd
taek2222 <oot309@daum.net>
2024-09-14 23:22:43 +0900

    refactor: Global 예외 핸들러에 체크, 언체크 예외 추가 (#93)

--- src/main/java/com/codenear/butterfly/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codenear/butterfly/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.codenear.butterfly.global.exception;
 
 import com.codenear.butterfly.global.dto.ResponseDTO;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,24 +17,34 @@ import static com.codenear.butterfly.global.util.ResponseUtil.createErrorRespons
 @Slf4j
 public class GlobalExceptionHandler {
 
-    public static final String errorMessage = "에러 발생 : {}";
+    public static final String ERROR_MESSAGE = "에러 발생 : {}";
+    public static final String FIELD = "field";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ResponseDTO> validationExceptions(MethodArgumentNotValidException ex) {
+        FieldError fieldError = ex.getBindingResult().getFieldErrors().get(0);
+        Map<String, String> filed = new HashMap<>();
+        filed.put(FIELD, fieldError.getField());
 
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult()
-                .getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-
-        log.warn(errorMessage, ex.getMessage());
-
-        return createErrorResponse(ErrorCode.VALIDATION_FAILED, errors);
+        log.warn(ERROR_MESSAGE, ex.getMessage(), ex);
+        return createErrorResponse(ErrorCode.VALIDATION_FAILED, fieldError.getDefaultMessage(), filed);
     }
 
     @ExceptionHandler(BusinessBaseException.class)
-    public ResponseEntity<ResponseDTO> businessException(BusinessBaseException ex, HttpServletResponse response) {
-        log.warn(errorMessage, ex.getMessage());
+    public ResponseEntity<ResponseDTO> businessException(BusinessBaseException ex) {
+        log.warn(ERROR_MESSAGE, ex.getMessage(), ex);
         return createErrorResponse(ex.getErrorCode(), ex.getBody());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDTO> checkException(Exception ex) {
+        log.error(ERROR_MESSAGE, ex.getMessage(), ex);
+        return createErrorResponse(ErrorCode.SERVER_ERROR, null);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ResponseDTO> uncheckException(Exception ex) {
+        log.error(ERROR_MESSAGE, ex.getMessage(), ex);
+        return createErrorResponse(ErrorCode.SERVER_ERROR, null);
     }
 }
