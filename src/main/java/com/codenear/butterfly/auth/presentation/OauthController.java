ee412fda7e64a1f466205f99c25c547bd0e22e8c
taek2222 <oot309@daum.net>
2024-09-14 22:21:02 +0900

    feat: Oauth 컨트롤러 Swagger 추가 (#91)

--- src/main/java/com/codenear/butterfly/auth/presentation/OauthController.java
+++ src/main/java/com/codenear/butterfly/auth/presentation/OauthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
-public class OauthController {
+public class OauthController implements OauthControllerSwagger {
     private final OauthService oauthService;
 
     @PostMapping("/login")
