a9dbac189ed8e23901cdd5c0d57d6cb2313f9460
taek2222 <oot309@daum.net>
2024-09-14 22:21:37 +0900

    refactor: DTO 부분 Swagger 수정 및 보완 (#91)

--- src/main/java/com/codenear/butterfly/auth/domain/dto/OauthDTO.java
+++ src/main/java/com/codenear/butterfly/auth/domain/dto/OauthDTO.java
@@ -4,12 +4,11 @@ import com.codenear.butterfly.member.domain.Platform;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
-@Schema(title = "소셜 로그인 API JSON", description = "소셜 로그인 JSON 데이터 입니다.")
+@Schema(title = "소셜 로그인 JSON", description = "소셜 로그인 전달 데이터")
 @Getter
 public class OauthDTO {
 
@@ -18,12 +17,12 @@ public class OauthDTO {
     @NotNull(message = "이메일은 필수 입력 항목입니다.")
     private String email;
 
-    @Schema(description = "Oauth 사용자의 고유 ID")
-    @NotBlank(message = "Oauth ID는 필수 입력 항목입니다.")
+    @Schema(description = "Oauth 사용자의 고유 ID", example = "123456789")
+    @NotNull(message = "Oauth ID는 필수 입력 항목입니다.")
     private String oauthId;
 
-    @Schema(description = "가입 경로", examples = {"GOOGLE", "KAKAO", "google", "kakao"})
+    @Schema(description = "가입 경로", example = "GOOGLE")
     @Enumerated(EnumType.STRING)
-    @NotBlank(message = "가입 경로가 올바르지 않습니다.")
+    @NotNull(message = "가입 경로가 올바르지 않습니다.")
     private Platform platform;
 }
