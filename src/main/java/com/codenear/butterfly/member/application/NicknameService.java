55967471b305fc234af40f747b766752d922db76
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-14 11:03:21 +0900

    refactor: 닉네임 생성 예외 처리

--- src/main/java/com/codenear/butterfly/member/application/NicknameService.java
+++ src/main/java/com/codenear/butterfly/member/application/NicknameService.java
@@ -1,8 +1,11 @@
 package com.codenear.butterfly.member.application;
 
+import com.codenear.butterfly.global.exception.ErrorCode;
 import com.codenear.butterfly.member.domain.repository.MemberRepository;
+import com.codenear.butterfly.member.exception.MemberException;
 import com.codenear.butterfly.member.util.NicknameList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -16,33 +19,49 @@ public class NicknameService {
     private final MemberRepository memberRepository;
 
     public Map<String, String> nicknameGenerator() {
-        String baseNickname = generateBaseNickname();
+        try {
+            String baseNickname = generateBaseNickname();
 
-        if (!isNicknameExists(baseNickname)) {
-            return createResponse(baseNickname);
+            if (!isNicknameExists(baseNickname)) {
+                return createResponse(baseNickname);
+            }
+
+            int maxNumber = findMaxNumberedNickname(baseNickname)
+                    .map(this::extractNumberFromNickname)
+                    .orElse(0);
+
+            String generatedNickname = maxNumber == 0 ? baseNickname + "1" : baseNickname + (maxNumber + 1);
+            return createResponse(generatedNickname);
+        } catch (RuntimeException e) {
+            throw new MemberException(ErrorCode.NICKNAME_GENERATION_FAILED, null);
         }
-
-        int maxNumber = findMaxNumberedNickname(baseNickname)
-                .map(this::extractNumberFromNickname)
-                .orElse(0);
-
-        String generatedNickname = maxNumber == 0 ? baseNickname + "1" : baseNickname + (maxNumber + 1);
-        return createResponse(generatedNickname);
     }
 
     private Optional<String> findMaxNumberedNickname(String baseNickname) {
-        return memberRepository.findMaxNumberedNickname(baseNickname);
+        try {
+            return memberRepository.findMaxNumberedNickname(baseNickname);
+        } catch (DataAccessException e) {
+            throw new MemberException(ErrorCode.DATABASE_ERROR, baseNickname);
+        }
     }
 
     private int extractNumberFromNickname(String nickname) {
-        return Optional.of(nickname.replaceAll("\\D+", ""))  // 숫자만 추출
-                .filter(s -> !s.isEmpty())
-                .map(Integer::parseInt)
-                .orElse(0);
+        try {
+            return Optional.of(nickname.replaceAll("\\D+", ""))  // 숫자만 추출
+                    .filter(s -> !s.isEmpty())
+                    .map(Integer::parseInt)
+                    .orElse(0);
+        } catch (NumberFormatException e) {
+            throw new MemberException(ErrorCode.INVALID_NICKNAME_FORMAT, nickname);
+        }
     }
 
     private boolean isNicknameExists(String baseNickname) {
-        return memberRepository.findMaxNumberedNickname(baseNickname).isPresent();
+        try {
+            return memberRepository.findMaxNumberedNickname(baseNickname).isPresent();
+        } catch (DataAccessException e) {
+            throw new MemberException(ErrorCode.DATABASE_ERROR, baseNickname);
+        }
     }
 
     private String generateBaseNickname() {
