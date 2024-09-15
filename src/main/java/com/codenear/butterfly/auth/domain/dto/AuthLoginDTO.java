1e925685a5ce9419a180a3c0ca39e1bde1cdd428
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-15 13:34:46 +0900

    refactor: NotNull 메시지 추가

--- src/main/java/com/codenear/butterfly/auth/domain/dto/AuthLoginDTO.java
+++ src/main/java/com/codenear/butterfly/auth/domain/dto/AuthLoginDTO.java
@@ -14,11 +14,11 @@ public class AuthLoginDTO {
 
     @Schema(description = "로그인 이메일", example = "codenear@naver.com")
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "올바른 이메일 형식이 아닙니다.")
-    @NotNull
+    @NotNull(message = "이메일을 입력해주세요.")
     private String email;
 
     @Schema(description = "로그인 비밀번호", example = "password")
-    @NotNull
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
 
     @Schema(description = "가입 경로 종류", examples = {"GOOGLE", "KAKAO", "CODENEAR"})
