d81b568147cf63858d17475a8c31b432b6aad3bf
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-15 18:45:10 +0900

    refactor: 닉네임 컨트롤러 네이밍 변경

--- src/main/java/com/codenear/butterfly/member/presentation/NicknameController.java
+++ src/main/java/com/codenear/butterfly/member/presentation/NicknameController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
-public class NicknameApi implements NicknameApiSwagger {
+public class NicknameController implements NicknameApiSwagger {
 
     private final NicknameService nicknameService;
 
