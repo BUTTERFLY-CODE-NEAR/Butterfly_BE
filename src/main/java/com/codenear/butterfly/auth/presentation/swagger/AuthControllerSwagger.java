8bc4ec98fd6804fd87d59b3aa999408adebe0314
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-16 18:28:25 +0900

    refactor: Swagger 로그인 관련 상태코드 수정

--- src/main/java/com/codenear/butterfly/auth/presentation/swagger/AuthControllerSwagger.java
+++ src/main/java/com/codenear/butterfly/auth/presentation/swagger/AuthControllerSwagger.java
@@ -27,7 +27,7 @@ public interface AuthControllerSwagger {
 
     @Operation(summary = "로그인 API", description = "개인(CODE NEAR) 로그인 API(Access, Refresh 토큰 발급)")
     @ApiResponses({
-            @ApiResponse(responseCode = "40102", description = "아이디 혹은 비밀번호가 불일치"),
+            @ApiResponse(responseCode = "40300", description = "아이디 혹은 비밀번호가 불일치"),
     })
     @PostMapping(value = "/login")
     ResponseEntity<ResponseDTO> login(AuthLoginDTO requestDTO, HttpServletResponse response);
@@ -35,8 +35,7 @@ public interface AuthControllerSwagger {
     @Operation(summary = "토큰 재발급 API", description = "Access, Refresh 토큰 재발급 <br> 요청시 **Refresh 쿠키 전달 필수**")
     @ApiResponses({
             @ApiResponse(responseCode = "40100", description = "발급 과정 오류가 발생, 토큰 재발급 요청으로 Access, Refresh 토큰 재발급 진행 필요"),
-            @ApiResponse(responseCode = "40101", description = "발급 과정 오류가 발생, Refresh 토큰 이상으로 재로그인 필요"),
-            @ApiResponse(responseCode = "40300", description = "이메일 혹은 비밀번호 불일치"),
+            @ApiResponse(responseCode = "40101", description = "발급 과정 오류가 발생, Refresh 토큰 이상으로 재로그인 필요")
     })
     @PostMapping("/reissue")
     ResponseEntity<ResponseDTO> reissue(HttpServletRequest request, HttpServletResponse response);
