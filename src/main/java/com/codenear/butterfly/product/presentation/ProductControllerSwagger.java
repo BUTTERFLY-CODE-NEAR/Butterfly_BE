6d9331ffc005af3d934d153a8d52a5b388345764
taek2222 <oot309@daum.net>
2024-09-21 21:17:20 +0900

    refactor: 제품 API 명세 리팩토링 (#119)

--- src/main/java/com/codenear/butterfly/product/presentation/ProductControllerSwagger.java
+++ src/main/java/com/codenear/butterfly/product/presentation/ProductControllerSwagger.java
@@ -1,13 +1,24 @@
 package com.codenear.butterfly.product.presentation;
 
 import com.codenear.butterfly.global.dto.ResponseDTO;
+import com.codenear.butterfly.member.domain.dto.MemberInfoDTO;
+import com.codenear.butterfly.product.domain.dto.ProductViewDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
-@Tag(name = "Product", description = "상품 정보 API")
+@Tag(name = "Product", description = "**(전체) 상품 정보 API**")
 public interface ProductControllerSwagger {
 
-    @Operation(summary = "상품 정보", description = "**[메인 / 카테고리]** 화면에 포함된 상품 정보")
+    @Operation(summary = "(전체) 상품 정보", description = "[카테고리] (전체) 상품 정보 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "body", description = "응답 메시지 예시",
+                    content = @Content(schema = @Schema(implementation = ProductViewDTO.class))),
+            @ApiResponse(responseCode = "200", description = "Success")
+    })
     ResponseEntity<ResponseDTO> productInfo();
 }
