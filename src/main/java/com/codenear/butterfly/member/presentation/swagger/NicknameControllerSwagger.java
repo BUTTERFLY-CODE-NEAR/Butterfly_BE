d1fab04a89c0eb4abfc18dd00ff07404f443900d
taek2222 <oot309@daum.net>
2024-09-21 20:40:56 +0900

    refactor: 유저 API 명세 리팩토링 (#119)

--- src/main/java/com/codenear/butterfly/member/presentation/swagger/NicknameControllerSwagger.java
+++ src/main/java/com/codenear/butterfly/member/presentation/swagger/NicknameControllerSwagger.java
@@ -1,15 +1,23 @@
 package com.codenear.butterfly.member.presentation.swagger;
 
 import com.codenear.butterfly.global.dto.ResponseDTO;
+import com.codenear.butterfly.member.domain.dto.NicknameDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Tag(name = "Member", description = "닉네임 생성 Api")
+@Tag(name = "Nickname", description = "**닉네임 생성 API**")
 public interface NicknameControllerSwagger {
 
-    @Operation(summary = "닉네임 생성", description = "랜덤 닉네임 생성 API(닉네임 중복 시 숫자 태그 추가)")
-    @GetMapping(value = "/nickname/generate")
+    @Operation(summary = "닉네임 생성", description = "랜덤 닉네임 생성 API **(중복일 때 숫자 태그 자동 추가)**")
+    @ApiResponses({
+            @ApiResponse(responseCode = "body", description = "응답 메시지 예시",
+                    content = @Content(schema = @Schema(implementation = NicknameDTO.class))),
+            @ApiResponse(responseCode = "200", description = "Success")
+    })
     ResponseEntity<ResponseDTO> nicknameGenerate();
 }
