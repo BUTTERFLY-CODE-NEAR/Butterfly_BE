3121232b526702e233bdcfd1fa764aa3b78630ec
taek2222 <oot309@daum.net>
2024-09-23 23:43:36 +0900

    refactor: API 매핑 리팩토링 작업 (#127)

--- src/main/java/com/codenear/butterfly/search/presentation/SearchController.java
+++ src/main/java/com/codenear/butterfly/search/presentation/SearchController.java
@@ -7,24 +7,22 @@ import com.codenear.butterfly.search.application.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/search")
 public class SearchController {
     private final SearchService searchService;
 
-    @PostMapping("/search/log")
+    @PostMapping
     public ResponseEntity<ResponseDTO> addSearchLog(@RequestParam String keyword, @AuthenticationPrincipal Member member) {
         searchService.addSearchLog(keyword, member);
         return ResponseUtil.createSuccessResponse(null);
     }
 
-    @GetMapping("/search/list")
-    public ResponseEntity<ResponseDTO> getSearchList(@AuthenticationPrincipal Member member) {
+    @GetMapping
+    public ResponseEntity<ResponseDTO> getSearchLogList(@AuthenticationPrincipal Member member) {
         return ResponseUtil.createSuccessResponse(searchService.getSearchList(member));
     }
 }
