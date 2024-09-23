5905e3b08b1cf2dc18ef03496b159a6bafefc424
taek2222 <oot309@daum.net>
2024-09-23 23:44:32 +0900

    refactor: 메서드화 및 상수 처리와 중복 처리 (#127)

--- src/main/java/com/codenear/butterfly/search/application/SearchService.java
+++ src/main/java/com/codenear/butterfly/search/application/SearchService.java
@@ -8,24 +8,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
+    public static final String SEARCH_LOG_KEY_PREFIX = "search_user:";
+    public static final int SEARCH_LOG_MAX_SIZE = 5;
+
     private final RedisTemplate<String, Object> redisTemplate;
     private final MemberRepository memberRepository;
 
     public void addSearchLog(String keyword, Member loginMember) {
         Member member = getMember(loginMember);
+        String key = getKey(member);
 
-        String key = "search_user:" + member.getId();
-        redisTemplate.opsForList().leftPush(key, keyword);
-        redisTemplate.opsForList().trim(key, 0, 9);
+        redisTemplate.opsForSet().add(key, keyword);
+
+        // 저장 검색어가 최대일 경우 삭제
+        Long size = redisTemplate.opsForSet().size(key);
+        if (size != null && size > SEARCH_LOG_MAX_SIZE)
+            redisTemplate.opsForSet().pop(key);
     }
 
-    public List<Object> getSearchList(Member loginMember) {
+    public Set<Object> getSearchList(Member loginMember) {
         Member member = getMember(loginMember);
+        return redisTemplate.opsForSet().members(getKey(member));
+    }
 
         String key = "search_user:" + member.getId();
         return redisTemplate.opsForList().range(key, 0, -1);
@@ -34,5 +43,9 @@ public class SearchService {
     private Member getMember(Member loginMember) {
         return memberRepository.findByEmailAndPlatform(loginMember.getEmail(), loginMember.getPlatform())
                 .orElseThrow(() -> new MemberException(ErrorCode.SERVER_ERROR, null));
+    }
+
+    private static String getKey(Member member) {
+        return SEARCH_LOG_KEY_PREFIX + member.getId();
     }
 }
