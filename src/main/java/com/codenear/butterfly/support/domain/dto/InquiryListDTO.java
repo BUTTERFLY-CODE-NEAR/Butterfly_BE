96f1a57384110c4f766a62c8affb40c4652a721e
taek2222 <oot309@daum.net>
2024-09-28 00:34:14 +0900

    fix: 고객 문의 리스트 날짜 추가 (#142)

--- src/main/java/com/codenear/butterfly/support/domain/dto/InquiryListDTO.java
+++ src/main/java/com/codenear/butterfly/support/domain/dto/InquiryListDTO.java
@@ -3,10 +3,13 @@ package com.codenear.butterfly.support.domain.dto;
 import com.codenear.butterfly.support.domain.InquiryStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDate;
+
 @Schema(title = "고객 문의 리스트 JSON", description = "고객 문의 리스트 요청 시 반환되는 응답 JSON 데이터 입니다.")
 public record InquiryListDTO(
         @Schema(description = "문의 순번") Long id,
         @Schema(description = "문의 내용") String inquiryContent,
         @Schema(description = "문의 응답") String responseContent,
-        @Schema(description = "문의 상태") InquiryStatus status) {
+        @Schema(description = "문의 상태") InquiryStatus status,
+        @Schema(description = "문의 날짜") LocalDate date) {
 }
