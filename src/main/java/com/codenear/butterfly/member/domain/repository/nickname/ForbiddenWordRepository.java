ad209c092c6fc288c57be241380aff75d2f53cf6
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-09-30 14:58:11 +0900

    fix: 누락된 어노테이션 추가

--- src/main/java/com/codenear/butterfly/member/domain/repository/nickname/ForbiddenWordRepository.java
+++ src/main/java/com/codenear/butterfly/member/domain/repository/nickname/ForbiddenWordRepository.java
@@ -2,7 +2,9 @@ package com.codenear.butterfly.member.domain.repository.nickname;
 
 import com.codenear.butterfly.member.domain.ForbiddenWord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ForbiddenWordRepository extends JpaRepository<ForbiddenWord, Long> {
 
 }
