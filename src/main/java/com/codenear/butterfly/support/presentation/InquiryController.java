84e0ed1194331127845c331bbf679d29429e4f13
taek2222 <oot309@daum.net>
2024-10-03 23:02:59 +0900

    refactor: 고객 문의 API 새로운 객체로 리팩토링 (#168)

--- src/main/java/com/codenear/butterfly/support/presentation/InquiryController.java
+++ src/main/java/com/codenear/butterfly/support/presentation/InquiryController.java
@@ -1,7 +1,7 @@
 package com.codenear.butterfly.support.presentation;
 
 import com.codenear.butterfly.global.dto.ResponseDTO;
-import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.member.domain.dto.MemberDTO;
 import com.codenear.butterfly.support.application.InquiryService;
 import com.codenear.butterfly.support.domain.dto.InquiryListDTO;
 import com.codenear.butterfly.support.domain.dto.InquiryRegisterDTO;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.codenear.butterfly.global.util.ResponseUtil.*;
+import static com.codenear.butterfly.global.util.ResponseUtil.createSuccessResponse;
 
 @RestController
 @RequestMapping("/support/inquiry")
@@ -22,14 +22,14 @@ public class InquiryController implements InquiryControllerSwagger {
     private final InquiryService inquiryService;
 
     @GetMapping
-    public ResponseEntity<ResponseDTO> getInquiryList(@AuthenticationPrincipal Member loginMember) {
-        List<InquiryListDTO> inquiryList = inquiryService.getInquiryList(loginMember);
+    public ResponseEntity<ResponseDTO> getInquiryList(@AuthenticationPrincipal MemberDTO memberDTO) {
+        List<InquiryListDTO> inquiryList = inquiryService.getInquiryList(memberDTO);
         return createSuccessResponse(inquiryList);
     }
 
     @PostMapping
-    public ResponseEntity<ResponseDTO> registerInquiry(@Valid @RequestBody InquiryRegisterDTO dto, @AuthenticationPrincipal Member loginMember) {
-        inquiryService.registerInquiry(dto, loginMember);
+    public ResponseEntity<ResponseDTO> registerInquiry(@Valid @RequestBody InquiryRegisterDTO inquiryRegisterDTO, @AuthenticationPrincipal MemberDTO memberDTO) {
+        inquiryService.registerInquiry(inquiryRegisterDTO, memberDTO);
         return createSuccessResponse(null);
     }
 }
