b16c98572a81277e209b32b1290b067bc4ff284c
taek2222 <oot309@daum.net>
2024-10-07 23:54:12 +0900

    feat: 응답 및 요청 DTO 정의 (#177)

---
+++ src/main/java/com/codenear/butterfly/address/domain/dto/AddressCreateDTO.java
@@ -0,0 +1,25 @@
+package com.codenear.butterfly.address.domain.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Schema(title = "주소 추가 JSON", description = "주소 추가 시 정의되는 JSON 데이터 입니다.")
+@Getter
+public class AddressCreateDTO {
+
+    @Schema(description = "주소 이름", example = "집")
+    @NotNull
+    private String addressName;
+
+    @Schema(description = "주소", example = "강릉시 구원로 54")
+    @NotNull
+    private String address;
+
+    @Schema(description = "상세 주소", example = "3층 302호")
+    @NotNull
+    private String detailedAddress;
+
+    @Schema(description = "현관 비밀번호", example = "#1234")
+    private String entrancePassword;
+}
---
+++ src/main/java/com/codenear/butterfly/address/domain/dto/AddressUpdateDTO.java
@@ -0,0 +1,28 @@
+package com.codenear.butterfly.address.domain.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Schema(title = "주소 수정 JSON", description = "주소 수정 시 정의되는 JSON 데이터 입니다.")
+@Getter
+public class AddressUpdateDTO {
+
+    @Schema(description = "주소 ID")
+    private Long id;
+
+    @Schema(description = "주소 이름", example = "집 이동")
+    @NotNull
+    private String addressName;
+
+    @Schema(description = "주소", example = "강릉시 구원로 54")
+    @NotNull
+    private String address;
+
+    @Schema(description = "상세 주소", example = "4층 403호")
+    @NotNull
+    private String detailedAddress;
+
+    @Schema(description = "현관 비밀번호", example = "#5678")
+    private String entrancePassword;
+}
