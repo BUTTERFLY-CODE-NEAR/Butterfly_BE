647b7de6583f66980983211b472b903190f8aa47
taek2222 <oot309@daum.net>
2024-10-07 23:53:37 +0900

    feat: Repository 반환 타입 변경 (#177)

--- src/main/java/com/codenear/butterfly/address/domain/AddressRepository.java
+++ src/main/java/com/codenear/butterfly/address/domain/AddressRepository.java
@@ -2,8 +2,8 @@ package com.codenear.butterfly.address.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.LinkedList;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    List<Address> findAllByMemberId(Long memberId);
+    LinkedList<Address> findAllByMemberId(Long memberId);
 }
