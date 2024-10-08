f4d09edf858ce2fb78f9d7c5fcaa11872cd2cfcf
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-08 17:29:04 +0900

    refactor: 상품 옵션 컬럼 추가

---
+++ src/main/java/com/codenear/butterfly/product/domain/dto/OptionDTO.java
@@ -0,0 +1,17 @@
+package com.codenear.butterfly.product.domain.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.math.BigDecimal;
+
+@Schema(title = "상품 옵션 JSON", description = "상품 옵션 정보 요청 시 반환되는 응답 JSON 데이터 입니다.")
+public record OptionDTO(
+    @Schema(description = "옵션 ID") Long optionId,
+    @Schema(description = "상품 제조사 및 판매처") String subtitle,
+    @Schema(description = "상품 이름") String productName,
+    @Schema(description = "상품 이미지", example = "http://example.com/profile.jpg") String productImage,
+    @Schema(description = "상품 원가") Integer originalPrice,
+    @Schema(description = "할인률 (%)") BigDecimal saleRate,
+    @Schema(description = "상품 할인가") Integer salePrice
+) {
+}
