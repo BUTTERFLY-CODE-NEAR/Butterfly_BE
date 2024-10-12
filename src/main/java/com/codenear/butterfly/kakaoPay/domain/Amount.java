8911d4527ed5ed0832d02a08c66bb446b51671da
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-12 15:14:31 +0900

    feat: 주문내역 저장 엔티티 추가

---
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/Amount.java
@@ -0,0 +1,21 @@
+package com.codenear.butterfly.kakaoPay.domain;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+
+@Entity
+@Setter
+public class Amount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long amountId;
+    private Integer total; // 총 결제 금액
+    private Integer taxFree; // 비과세 금액
+    private Integer vat; // 부가세 금액
+    private Integer point; // 사용한 포인트 금액
+    private Integer discount; // 할인 금액
+
+    @OneToOne(mappedBy = "amount")
+    private SinglePayment singlePayment;
+}
---
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/CardInfo.java
@@ -0,0 +1,30 @@
+package com.codenear.butterfly.kakaoPay.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CardInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardInfoId;
+    private String kakaopayPurchaseCorp; // 카카오페이 매입사명
+    private String kakaopayPurchaseCorpCode; // 카카오페이 매입사 코드
+    private String kakaopayIssuerCorp; // 카카오페이 발급사명
+    private String kakaopayIssuerCorpCode; // 카카오페이 발급사 코드
+    private String bin; // 카드 BIN
+    private String cardType; // 카드 타입
+    private String installMonth; // 할부 개월 수
+    private String approvedId; // 카드사 승인번호
+    private String cardMid; // 카드사 가맹점 번호
+    private String interestFreeInstall; // 무이자할부 여부(Y/N)
+    private String installmentType; // 할부 유형
+    private String cardItemCode; // 카드 상품 코드
+
+    @OneToOne(mappedBy = "cardInfo")
+    private SinglePayment singlePayment;
+}
