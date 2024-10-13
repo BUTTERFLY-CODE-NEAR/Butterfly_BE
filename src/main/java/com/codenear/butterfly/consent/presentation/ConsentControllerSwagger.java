cc8a7d27e1083df2bf03c48909e03649ed59d90a
taek2222 <oot309@daum.net>
2024-10-13 17:44:32 +0900

    feat: 수신 동의 변경 Controller, API 문서화 추가 (#199)

--- src/main/java/com/codenear/butterfly/consent/presentation/ConsentControllerSwagger.java
+++ src/main/java/com/codenear/butterfly/consent/presentation/ConsentControllerSwagger.java
@@ -1,6 +1,7 @@
 package com.codenear.butterfly.consent.presentation;
 
 import com.codenear.butterfly.consent.dto.ConsentInfoResponseDTO;
+import com.codenear.butterfly.consent.dto.ConsentUpdateRequestDTO;
 import com.codenear.butterfly.global.dto.ResponseDTO;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,8 +10,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Consent", description = "**수신 동의 API**")
 public interface ConsentControllerSwagger {
@@ -22,4 +25,7 @@ public interface ConsentControllerSwagger {
             @ApiResponse(responseCode = "200", description = "Success")
     })
     ResponseEntity<ResponseDTO> getConsents(@AuthenticationPrincipal MemberDTO memberDTO);
+
+    @Operation(summary = "수신 동의 변경", description = "수신 동의 변경 API")
+    ResponseEntity<ResponseDTO> updateConsent(@Valid @RequestBody ConsentUpdateRequestDTO updateRequestDTO, @AuthenticationPrincipal MemberDTO memberDTO);
 }
