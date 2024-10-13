f73f0e8da51c0c6b5a80cd39fde341a52cb41055
taek2222 <oot309@daum.net>
2024-10-13 15:03:06 +0900

    feat: 수신 관련 동의 데이터 테이블 정의 (#199)

---
+++ src/main/java/com/codenear/butterfly/consent/domain/Consent.java
@@ -0,0 +1,26 @@
+package com.codenear.butterfly.consent.domain;
+
+import com.codenear.butterfly.global.domain.BaseEntity;
+import com.codenear.butterfly.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class Consent extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ConsentType consentType;
+
+    private boolean isAgreed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
--- src/main/java/com/codenear/butterfly/kakaoPay/application/SinglePaymentService.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/application/SinglePaymentService.java
@@ -20,7 +20,6 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
