78dc3b7bfb59705360c74034065b10955232f0e6
taek2222 <oot309@daum.net>
2024-10-13 17:05:22 +0900

    feat: 수신 동의 목록 반환 서비스 로직 추가 (#199)

---
+++ src/main/java/com/codenear/butterfly/consent/application/ConsentService.java
@@ -0,0 +1,51 @@
+package com.codenear.butterfly.consent.application;
+
+import com.codenear.butterfly.consent.domain.Consent;
+import com.codenear.butterfly.consent.domain.ConsentRepository;
+import com.codenear.butterfly.consent.domain.ConsentType;
+import com.codenear.butterfly.consent.dto.ConsentInfoResponseDTO;
+import com.codenear.butterfly.consent.dto.ConsentSingleResponseDTO;
+import com.codenear.butterfly.member.domain.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ConsentService {
+    private final ConsentRepository consentRepository;
+
+    public ConsentInfoResponseDTO getConsentsInfo(MemberDTO memberDTO) {
+        List<Consent> consents = loadConsentsByMemberId(memberDTO);
+        List<ConsentSingleResponseDTO> consentSingleDTOS = getConsentSingleResponseDTOS(consents);
+        return new ConsentInfoResponseDTO(consentSingleDTOS);
+    }
+
+    private List<ConsentSingleResponseDTO> getConsentSingleResponseDTOS(List<Consent> consents) {
+        List<ConsentSingleResponseDTO> responseDTOList = new ArrayList<>();
+
+        for (ConsentType type : ConsentType.values()) {
+            boolean agreed = isAgreed(consents, type);
+            responseDTOList.add(createConsentSingleResponseDTO(type, agreed));
+        }
+        return responseDTOList;
+    }
+
+    private boolean isAgreed(List<Consent> consents, ConsentType type) {
+        return consents.stream()
+                .filter(consent -> consent.getConsentType().equals(type))
+                .map(Consent::isAgreed)
+                .findFirst()
+                .orElse(false);
+    }
+
+    private  ConsentSingleResponseDTO createConsentSingleResponseDTO(ConsentType value, boolean agreed) {
+        return new ConsentSingleResponseDTO(value, agreed);
+    }
+
+    private List<Consent> loadConsentsByMemberId(MemberDTO memberDTO) {
+        return consentRepository.findByMemberId(memberDTO.getId());
+    }
+}
