53674acbdbef8763e10ac01aa69dde112514936d
taek2222 <oot309@daum.net>
2024-10-13 17:45:14 +0900

    feat: 수신 동의 변경 비즈니스 로직 추가 (#199)

--- src/main/java/com/codenear/butterfly/consent/application/ConsentService.java
+++ src/main/java/com/codenear/butterfly/consent/application/ConsentService.java
@@ -5,17 +5,23 @@ import com.codenear.butterfly.consent.domain.ConsentRepository;
 import com.codenear.butterfly.consent.domain.ConsentType;
 import com.codenear.butterfly.consent.dto.ConsentInfoResponseDTO;
 import com.codenear.butterfly.consent.dto.ConsentSingleResponseDTO;
+import com.codenear.butterfly.consent.dto.ConsentUpdateRequestDTO;
+import com.codenear.butterfly.member.application.MemberService;
+import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ConsentService {
     private final ConsentRepository consentRepository;
+    private final MemberService memberService;
 
     public ConsentInfoResponseDTO getConsentsInfo(MemberDTO memberDTO) {
         List<Consent> consents = loadConsentsByMemberId(memberDTO);
@@ -47,5 +53,26 @@ public class ConsentService {
 
     private List<Consent> loadConsentsByMemberId(MemberDTO memberDTO) {
         return consentRepository.findByMemberId(memberDTO.getId());
+    }
+
+    public void updateConsent(ConsentUpdateRequestDTO updateRequestDTO, MemberDTO memberDTO) {
+        List<Consent> consents = loadConsentsByMemberId(memberDTO);
+        ConsentType type = updateRequestDTO.getConsentType();
+
+        Consent consent = consents.stream()
+                .filter(findConsent -> findConsent.getConsentType().equals(type))
+                .findFirst()
+                .orElseGet(() -> createConsent(type, false, memberService.loadMemberByMemberId(memberDTO.getId())));
+
+        consent.toggleAgreement();
+        consentRepository.save(consent);
+    }
+
+    private Consent createConsent(ConsentType type, boolean agreed, Member member) {
+        return Consent.builder()
+                .consentType(type)
+                .isAgreed(agreed)
+                .member(member)
+                .build();
     }
 }
