4ff7f042f4d8c368d70be01f4615866141073cc6
taek2222 <oot309@daum.net>
2024-10-13 16:07:12 +0900

    feat: 수신 동의 목록 반환 JPA 추가 (#199)

--- src/main/java/com/codenear/butterfly/consent/domain/ConsentRepository.java
+++ src/main/java/com/codenear/butterfly/consent/domain/ConsentRepository.java
@@ -2,5 +2,8 @@ package com.codenear.butterfly.consent.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsentRepository extends JpaRepository<Consent, Long> {
+    List<Consent> findByMemberId(Long memberId);
 }
