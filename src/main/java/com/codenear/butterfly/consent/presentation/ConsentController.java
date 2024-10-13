6e56e93f096091d58403bb4e6b756146ec6db951
taek2222 <oot309@daum.net>
2024-10-13 17:44:32 +0900

    feat: 수신 동의 변경 Controller, API 문서화 추가 (#199)

--- src/main/java/com/codenear/butterfly/consent/presentation/ConsentController.java
+++ src/main/java/com/codenear/butterfly/consent/presentation/ConsentController.java
@@ -1,15 +1,15 @@
 package com.codenear.butterfly.consent.presentation;
 
 import com.codenear.butterfly.consent.application.ConsentService;
+import com.codenear.butterfly.consent.dto.ConsentUpdateRequestDTO;
 import com.codenear.butterfly.global.dto.ResponseDTO;
 import com.codenear.butterfly.global.util.ResponseUtil;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consent")
@@ -20,5 +20,11 @@ public class ConsentController implements ConsentControllerSwagger {
     @GetMapping
     public ResponseEntity<ResponseDTO> getConsents(@AuthenticationPrincipal MemberDTO memberDTO) {
         return ResponseUtil.createSuccessResponse(consentService.getConsentsInfo(memberDTO));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ResponseDTO> updateConsent(@Valid @RequestBody ConsentUpdateRequestDTO updateRequestDTO, @AuthenticationPrincipal MemberDTO memberDTO) {
+        consentService.updateConsent(updateRequestDTO, memberDTO);
+        return ResponseUtil.createSuccessResponse(null);
     }
 }
