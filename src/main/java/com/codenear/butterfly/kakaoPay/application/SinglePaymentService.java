31538f48c2d94bfbae69e33672ca10568888be61
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-14 17:26:55 +0900

    refactor: 결제 승인 시 주문 유형에 따른 주문내역 저장 로직 추가

--- src/main/java/com/codenear/butterfly/kakaoPay/application/SinglePaymentService.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/application/SinglePaymentService.java
@@ -1,12 +1,19 @@
 package com.codenear.butterfly.kakaoPay.application;
 
+import com.codenear.butterfly.address.domain.Address;
+import com.codenear.butterfly.address.domain.AddressRepository;
+import com.codenear.butterfly.global.exception.ErrorCode;
 import com.codenear.butterfly.kakaoPay.domain.Amount;
 import com.codenear.butterfly.kakaoPay.domain.CardInfo;
+import com.codenear.butterfly.kakaoPay.domain.OrderDetails;
 import com.codenear.butterfly.kakaoPay.domain.SinglePayment;
+import com.codenear.butterfly.kakaoPay.domain.dto.OrderType;
 import com.codenear.butterfly.kakaoPay.domain.dto.PaymentRequestDTO;
 import com.codenear.butterfly.kakaoPay.domain.dto.kakao.ApproveResponseDTO;
 import com.codenear.butterfly.kakaoPay.domain.dto.kakao.ReadyResponseDTO;
+import com.codenear.butterfly.kakaoPay.domain.repository.OrderDetailsRepository;
 import com.codenear.butterfly.kakaoPay.domain.repository.SinglePaymentRepository;
+import com.codenear.butterfly.kakaoPay.exception.KakaoPayException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +24,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -33,12 +42,17 @@ public class SinglePaymentService {
     private String CID;
 
     @Value("${kakao.payment.secret-key-dev}")
-    private String secret_key;
+    private String secretKey;
 
     @Value("${kakao.payment.host}")
     private String host;
 
+    @Value("${kakao.payment.request-url}")
+    private String requestUrl;
+
     private final SinglePaymentRepository singlePaymentRepository;
+    private final AddressRepository addressRepository;
+    private final OrderDetailsRepository orderDetailsRepository;
     private final RedisTemplate<String, String> redisTemplate;
 
     public ReadyResponseDTO kakaoPayReady(PaymentRequestDTO paymentRequestDTO, Long memberId) {
@@ -54,9 +68,9 @@ public class SinglePaymentService {
         parameters.put("total_amount", paymentRequestDTO.getTotal());
         parameters.put("vat_amount", 0);
         parameters.put("tax_free_amount", 0);
-        parameters.put("approval_url", "http://localhost:8080/payment/success"); // 성공 시 redirect url http://localhost:8080/success?pg_token=2c44d553eb444534f36d
-        parameters.put("cancel_url", "http://localhost:8080/payment/cancel"); // 취소 시 redirect url
-        parameters.put("fail_url", "http://localhost:8080/payment/fail"); // 실패 시 redirect url
+        parameters.put("approval_url", requestUrl+"/payment/success");
+        parameters.put("cancel_url", requestUrl+"/payment/cancel");
+        parameters.put("fail_url", requestUrl+"/payment/fail");
 
         // 파라미터, 헤더
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(parameters, this.getHeaders());
@@ -77,13 +91,30 @@ public class SinglePaymentService {
         saveOrderId(memberId, partnerOrderId);
         saveTransactionId(memberId, Objects.requireNonNull(kakaoPayReady).getTid());
 
+        saveOrderTypeAndAddressId(memberId, paymentRequestDTO);
+
         return kakaoPayReady;
+    }
+
+    private void saveOrderTypeAndAddressId(Long memberId, PaymentRequestDTO paymentRequestDTO) {
+        // Redis에 주문 유형 저장
+        String orderTypeKey = "orderType:" + memberId;
+        redisTemplate.opsForValue().set(orderTypeKey, paymentRequestDTO.getOrderType(), 30, TimeUnit.MINUTES);
+
+        // 배달 주문일 경우 주소 ID 저장
+        if (OrderType.DELIVER.getType().equals(paymentRequestDTO.getOrderType())) {
+            String addressIdKey = "addressId:" + memberId;
+            redisTemplate.opsForValue().set(addressIdKey, paymentRequestDTO.getAddressId().toString(), 30, TimeUnit.MINUTES);
+        }
     }
 
     @Transactional
     public void approveResponse(String pgToken, Long memberId) {
         String orderId = getOrderId(memberId);
         String transactionId = getTransactionId(memberId);
+        String orderTypeString = getOrderType(memberId);
+        OrderType orderType = OrderType.fromType(orderTypeString);
+        Long addressId = getAddressId(memberId);
 
         // 카카오 요청
         Map<String, Object> parameters = new HashMap<>();
@@ -116,11 +147,103 @@ public class SinglePaymentService {
             singlePayment.setCardInfo(cardInfo);
         }
 
+        saveOrderDetails(orderType, addressId, approveResponseDTO);
+
         singlePaymentRepository.save(singlePayment);
 
-        // Redis에서 주문 ID와 거래 ID 삭제
+        // Redis에서 주문 관련 데이터 삭제
+        removeOrderRelatedData(memberId);
+    }
+
+    private void saveOrderDetails(OrderType orderType, Long addressId, ApproveResponseDTO approveResponseDTO) {
+        OrderDetails orderDetails = new OrderDetails();
+        orderDetails.setOrderType(orderType);
+
+        if (OrderType.PICKUP.getType().equals(orderType.getType())) {
+            orderDetails.setPickupPlace(approveResponseDTO.getItem_name());
+            orderDetails.setPickupDate(LocalDate.now());
+            orderDetails.setPickupTime(LocalTime.now());
+        } else if (OrderType.DELIVER.getType().equals(orderType.getType())) {
+            Address address = addressRepository.findById(addressId)
+                    .orElseThrow(() -> new KakaoPayException(ErrorCode.ADDRESS_NOT_FOUND, null));
+            orderDetails.setAddress(address);
+        }
+
+        orderDetailsRepository.save(orderDetails);
+    }
+
+    public void cancelPayment(Long memberId) {
+        removeOrderRelatedData(memberId);
+    }
+
+    public void failPayment(Long memberId) {
+        removeOrderRelatedData(memberId);
+    }
+
+    private void saveOrderId(Long memberId, String orderId) {
+        String key = "order:" + memberId;
+        redisTemplate.opsForValue().set(key, orderId, 30, TimeUnit.MINUTES);
+    }
+
+    public String getOrderId(Long memberId) {
+        String key = "order:" + memberId;
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    private void removeOrderId(Long memberId) {
+        String key = "order:" + memberId;
+        redisTemplate.delete(key);
+    }
+
+    private void saveTransactionId(Long memberId, String tid) {
+        String key = "transaction:" + memberId;
+        redisTemplate.opsForValue().set(key, tid, 30, TimeUnit.MINUTES);
+    }
+
+    private String getTransactionId(Long memberId) {
+        String key = "transaction:" + memberId;
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    private void removeTransactionId(Long memberId) {
+        String key = "transaction:" + memberId;
+        redisTemplate.delete(key);
+    }
+
+    private String getOrderType(Long memberId) {
+        String key = "orderType:" + memberId;
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    private Long getAddressId(Long memberId) {
+        String key = "addressId:" + memberId;
+        String addressIdStr = redisTemplate.opsForValue().get(key);
+        return addressIdStr != null ? Long.parseLong(addressIdStr) : null;
+    }
+
+    private void removeOrderRelatedData(Long memberId) {
         removeOrderId(memberId);
         removeTransactionId(memberId);
+        removeOrderType(memberId);
+        removeAddressId(memberId);
+    }
+
+    private void removeOrderType(Long memberId) {
+        String key = "orderType:" + memberId;
+        redisTemplate.delete(key);
+    }
+
+    private void removeAddressId(Long memberId) {
+        String key = "addressId:" + memberId;
+        redisTemplate.delete(key);
+    }
+
+    // 카카오가 요구한 헤더값
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", "SECRET_KEY " + secretKey);
+        return headers;
     }
 
     private SinglePayment getSinglePayment(ApproveResponseDTO approveResponseDTO) {
@@ -165,53 +288,5 @@ public class SinglePaymentService {
         cardInfo.setKakaopayIssuerCorp(approveResponseDTO.getCard_info().getKakaopay_issuer_corp());
         cardInfo.setKakaopayIssuerCorpCode(approveResponseDTO.getCard_info().getKakaopay_issuer_corp_code());
         return cardInfo;
-    }
-
-    public void cancelPayment(Long memberId) {
-        removeOrderId(memberId);
-        removeTransactionId(memberId);
-    }
-
-    public void failPayment(Long memberId) {
-        removeOrderId(memberId);
-        removeTransactionId(memberId);
-    }
-
-    private void saveOrderId(Long memberId, String orderId) {
-        String key = "order:" + memberId;
-        redisTemplate.opsForValue().set(key, orderId, 30, TimeUnit.MINUTES);
-    }
-
-    public String getOrderId(Long memberId) {
-        String key = "order:" + memberId;
-        return redisTemplate.opsForValue().get(key);
-    }
-
-    private void removeOrderId(Long memberId) {
-        String key = "order:" + memberId;
-        redisTemplate.delete(key);
-    }
-
-    private void saveTransactionId(Long memberId, String tid) {
-        String key = "transaction:" + memberId;
-        redisTemplate.opsForValue().set(key, tid, 30, TimeUnit.MINUTES);
-    }
-
-    private String getTransactionId(Long memberId) {
-        String key = "transaction:" + memberId;
-        return redisTemplate.opsForValue().get(key);
-    }
-
-    private void removeTransactionId(Long memberId) {
-        String key = "transaction:" + memberId;
-        redisTemplate.delete(key);
-    }
-
-    // 카카오가 요구한 헤더값
-    private HttpHeaders getHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("Authorization", "SECRET_KEY " + secret_key);
-        return headers;
     }
 }
