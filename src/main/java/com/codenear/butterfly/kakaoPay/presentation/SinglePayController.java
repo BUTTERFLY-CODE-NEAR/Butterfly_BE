391bbb6a393307aa76814e9834ae20e4d0b0d473
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-16 11:49:49 +0900

    refactor: 카카오페이 단건결제 API Swagger 수정

--- src/main/java/com/codenear/butterfly/kakaoPay/presentation/SinglePayController.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/presentation/SinglePayController.java
@@ -4,10 +4,10 @@ import com.codenear.butterfly.global.dto.ResponseDTO;
 import com.codenear.butterfly.global.exception.ErrorCode;
 import com.codenear.butterfly.global.util.ResponseUtil;
 import com.codenear.butterfly.kakaoPay.application.SinglePaymentService;
-import com.codenear.butterfly.kakaoPay.domain.dto.PaymentRequestDTO;
+import com.codenear.butterfly.kakaoPay.domain.dto.DeliveryPaymentRequestDTO;
+import com.codenear.butterfly.kakaoPay.domain.dto.PickupPaymentRequestDTO;
 import com.codenear.butterfly.kakaoPay.exception.KakaoPayException;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,29 +20,30 @@ public class SinglePayController implements SinglePayControllerSwagger {
 
     private final SinglePaymentService singlePaymentService;
 
-    //결제 요청
-    @PostMapping("/ready")
-    public ResponseEntity<ResponseDTO> paymentRequest(@RequestBody PaymentRequestDTO paymentRequestDTO,
-                                                      @AuthenticationPrincipal MemberDTO memberDTO) {
-        return ResponseUtil.createSuccessResponse(singlePaymentService.kakaoPayReady(paymentRequestDTO, memberDTO.getId())); // "redirect: https://online-pay.kakao.com/mockup/v1/80db866d0c2bbeb45c69d1f5603feb3be67e492edfefc0018bd2991c9237d68a/info";
+    @PostMapping("/ready/pickup")
+    public ResponseEntity<ResponseDTO> pickupPaymentRequest(@RequestBody PickupPaymentRequestDTO paymentRequestDTO,
+                                                            @AuthenticationPrincipal MemberDTO memberDTO) {
+        return ResponseUtil.createSuccessResponse(singlePaymentService.kakaoPayReady(paymentRequestDTO, memberDTO.getId(), "pickup"));
     }
 
-    // 예시: http://localahost:8080/success?pg_token=2c44d553eb444534f36d
-    // 결제 성공
+    @PostMapping("/ready/delivery")
+    public ResponseEntity<ResponseDTO> deliveryPaymentRequest(@RequestBody DeliveryPaymentRequestDTO paymentRequestDTO,
+                                                              @AuthenticationPrincipal MemberDTO memberDTO) {
+        return ResponseUtil.createSuccessResponse(singlePaymentService.kakaoPayReady(paymentRequestDTO, memberDTO.getId(), "deliver"));
+    }
+
     @GetMapping("/success")
     public void successPaymentRequest(@RequestParam("pg_token") String pgToken,
                                       @AuthenticationPrincipal MemberDTO memberDTO) {
         singlePaymentService.approveResponse(pgToken, memberDTO.getId());
     }
 
-    // 결제준비 취소
     @GetMapping("/cancel")
     public void cancelPaymentRequest(@AuthenticationPrincipal MemberDTO memberDTO) {
         singlePaymentService.cancelPayment(memberDTO.getId());
         throw new KakaoPayException(ErrorCode.PAY_CANCEL, null);
     }
 
-    // 결제 실패
     @GetMapping("/fail")
     public void failPaymentRequest(@AuthenticationPrincipal MemberDTO memberDTO) {
         singlePaymentService.failPayment(memberDTO.getId());
