ac7a68e91b4f18d887958674b29ea870a7ed11f0
taek2222 <oot309@daum.net>
2024-10-20 03:55:12 +0900

    feat: S3 요청 보낼 디렉토리 Enum 선언 (#227)
    S3 요청 디렉토리를 편리하게 Enum 으로 선언해 관리하려고 생성했습니다.

---
+++ src/main/java/com/codenear/butterfly/s3/domain/S3Directory.java
@@ -0,0 +1,14 @@
+package com.codenear.butterfly.s3.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum S3Directory {
+    PROFILE_IMAGE("image/");
+
+    private final String value;
+
+    S3Directory(String value) {
+        this.value = value;
+    }
+}
