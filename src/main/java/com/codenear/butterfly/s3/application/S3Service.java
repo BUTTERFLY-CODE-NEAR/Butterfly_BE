2cc51dc836fda88f5a86fb9bbfe0c2f913fceedf
taek2222 <oot309@daum.net>
2024-10-22 01:13:42 +0900

    feat: S3 파일 등록 기능 구현 (#229)
    S3의 파일 등록 기능을 구현했습니다.
    
    추후에 이미지 업로드 기능 개발 후 리팩토링 진행하겠습니다.

--- src/main/java/com/codenear/butterfly/s3/application/S3Service.java
+++ src/main/java/com/codenear/butterfly/s3/application/S3Service.java
@@ -1,11 +1,21 @@
 package com.codenear.butterfly.s3.application;
 
+import com.codenear.butterfly.global.exception.ErrorCode;
 import com.codenear.butterfly.s3.domain.S3Directory;
+import com.codenear.butterfly.s3.exception.S3Exception;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +25,53 @@ public class S3Service {
 
     private static final String BUKKIT_NAME = "codenear";
 
-    public void deleteFile(S3Directory directory, String fileName) {
-        String fileKey = directory.getValue() + fileName;
+    public String uploadFile(MultipartFile file, S3Directory directory) {
+        try {
+            String fileKey = createFileKey(directory);
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(BUKKIT_NAME)
+                    .key(fileKey)
+                    .build();
 
+            Path path = getPath(file);
+            byte[] bytes = get(path);
+
+            RequestBody requestBody = RequestBody.fromBytes(bytes);
+            amazonS3Client.putObject(putObjectRequest, requestBody);
+
+            deleteTempFile(path); // 임시 파일 삭제
+            return fileKey;
+        } catch (Exception e) {
+            throw new S3Exception(ErrorCode.SERVER_ERROR, null);
+        }
+    }
+
+    public void deleteFile(String fileKey) {
         DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
                 .bucket(BUKKIT_NAME)
                 .key(fileKey)
                 .build();
 
         amazonS3Client.deleteObject(deleteObjectRequest);
+    }
+
+    private void deleteTempFile(Path bytes) throws IOException {
+        Files.delete(bytes);
+    }
+
+    private String createFileKey(S3Directory directory) {
+        UUID randomUUID = UUID.randomUUID();
+        return directory.getValue() + randomUUID;
+    }
+    
+    private byte[] get(Path tempFile) throws IOException {
+        String string = tempFile.toString();
+        return Files.readAllBytes(Paths.get(string));
+    }
+
+    private Path getPath(MultipartFile file) throws IOException {
+        Path tempFile = Files.createTempFile("tempFile-", file.getOriginalFilename());
+        file.transferTo(tempFile.toFile());
+        return tempFile;
     }
 }
