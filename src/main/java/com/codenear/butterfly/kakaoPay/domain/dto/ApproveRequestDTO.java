60befcd9e9cadd0f55ef8daeb17e7ddca6fcbe7b
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-21 21:23:52 +0900

    refactor: 승인 요청 DTO record로 리팩토링

--- src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/ApproveRequestDTO.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/ApproveRequestDTO.java
@@ -1,15 +1,10 @@
 package com.codenear.butterfly.kakaoPay.domain.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+public record ApproveRequestDTO (
+        String tid,
+        String orderId,
+        String memberId,
+        String pgToken
+) {
 
-@Getter
-@Setter
-@ToString
-public class ApproveRequestDTO {
-    private String tid;
-    private String orderId;
-    private String memberId;
-    private String pgToken;
 }
