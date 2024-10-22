e466b41ab08a24cba0921436e96f505bca4e1d73
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-23 00:03:25 +0900

    feat: 결제 취소 관련 엔티티 추가

---
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/CancelPayment.java
@@ -0,0 +1,33 @@
+package com.codenear.butterfly.kakaoPay.domain;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@NoArgsConstructor
+public class CancelPayment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String aid; // 요청 고유 번호
+    private String tid; // 결제 고유 번호
+    private String cid; // 가맹점 코드
+    private String status;
+    private String partnerOrderId; // 가맹점 주문번호
+    private String partnerUserId; // 가맹점 회원 id
+    private String paymentMethodType; // 결제 수단(CARD 또는 MONEY)
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "canceled_payment_id")
+    private CanceledAmount canceledAmount;
+
+    private String itemName; // 상품 이름
+    private String itemCode; // 상품 코드
+    private Integer quantity; // 상품 수량
+    private String createdAt; // 결제 준비 요청 시간
+    private String approvedAt; // 결제 승인 시간
+    private String payload; // 결제 승인 요청에 대해 저장 값, 요청 시 전달된 내용
+}
