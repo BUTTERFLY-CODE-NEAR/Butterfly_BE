929c5cfd51719846d64ffc761f9206b9a5ff270c
taek2222 <oot309@daum.net>
2024-10-22 18:56:25 +0900

    feat(ProfileController): 이미지 추가로, 데이터 전달 방식 변경 (#225)
    - 이미지 추가로, 데이터 받는 방식을 변경했습니다.

--- src/main/java/com/codenear/butterfly/member/presentation/ProfileController.java
+++ src/main/java/com/codenear/butterfly/member/presentation/ProfileController.java
@@ -8,10 +8,11 @@ import com.codenear.butterfly.member.domain.dto.ProfileUpdateRequestDTO;
 import com.codenear.butterfly.member.presentation.swagger.ProfileControllerSwagger;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController implements ProfileControllerSwagger {
     private final ProfileService profileService;
 
-    @PatchMapping
-    public ResponseEntity<ResponseDTO> updateMemberProfile(@Valid @RequestBody ProfileUpdateRequestDTO memberProfileRequestDTO,
+    @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ResponseDTO> updateMemberProfile(@Valid @ModelAttribute ProfileUpdateRequestDTO memberProfileRequestDTO,
                                                            @AuthenticationPrincipal MemberDTO memberDTO) {
         profileService.updateMemberProfile(memberProfileRequestDTO, memberDTO);
         return ResponseUtil.createSuccessResponse(null);
