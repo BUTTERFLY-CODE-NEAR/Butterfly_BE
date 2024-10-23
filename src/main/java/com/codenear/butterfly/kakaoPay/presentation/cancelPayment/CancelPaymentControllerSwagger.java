3d56e97511555bdf55d2bc3626986fb68853d154
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-23 15:34:15 +0900

    feat(CancelPaymentControllerSwagger): 결제 취소 api swagger 작성

--- src/main/java/com/codenear/butterfly/kakaoPay/presentation/cancelPayment/CancelPaymentControllerSwagger.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/presentation/cancelPayment/CancelPaymentControllerSwagger.java
@@ -1,7 +1,18 @@
 package com.codenear.butterfly.kakaoPay.presentation.cancelPayment;
 
+import com.codenear.butterfly.global.dto.ResponseDTO;
+import com.codenear.butterfly.kakaoPay.domain.dto.CancelRequestDTO;
+import com.codenear.butterfly.member.domain.dto.MemberDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "CancelPayment", description = "**카카오페이 결제취소 API**")
 public interface CancelPaymentControllerSwagger {
+
+    @Operation(summary = "결제 취소(환불)", description = "결제 취소 API")
+    ResponseEntity<ResponseDTO> cancelPaymentRequest(@RequestBody CancelRequestDTO cancelRequestDTO,
+                                                     @AuthenticationPrincipal MemberDTO memberDTO);
 }
