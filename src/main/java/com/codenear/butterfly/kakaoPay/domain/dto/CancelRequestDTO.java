7db766cb122429a64fab1ae3a8b5063c2876c321
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-23 15:39:28 +0900

    refactor(CancelRequestDTO): 결제 취소 요청 DTO Swagger 수정

--- src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/CancelRequestDTO.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/CancelRequestDTO.java
@@ -3,9 +3,10 @@ package com.codenear.butterfly.kakaoPay.domain.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(title = "결제 취소 DTO", description = "결제 취소 시 사용되는 데이터입니다.")
-public record CancelRequestDTO (
-    String tid,
-    String cancelAmount
-) {
+public class CancelRequestDTO {
 
+    @Schema(description = "주문 id", example = "t로 시작하는 20자리 영어+숫자")
+    String tid;
+    @Schema(description = "결제 취소 금액", example = "30000")
+    String cancelAmount;
 }
