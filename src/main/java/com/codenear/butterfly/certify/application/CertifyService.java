feca989f27d7be4ab54c765a491bfe6a73c7b16d
taek2222 <oot309@daum.net>
2024-10-26 04:24:10 +0900

    refactor(CertifyService): 플랫폼 문자 메시지 템플릿 적용 (#246)
    - 문자 메시지 상수로 정의해 메시지 템플릿을 불러와 적용했습니다.
    
    - 메서드 분리로, 가독성을 향상했습니다.

--- src/main/java/com/codenear/butterfly/certify/application/CertifyService.java
+++ src/main/java/com/codenear/butterfly/certify/application/CertifyService.java
@@ -1,5 +1,7 @@
 package com.codenear.butterfly.certify.application;
 
+import static com.codenear.butterfly.certify.domain.CertifyMessage.*;
+
 import com.codenear.butterfly.certify.domain.dto.CertifyRequestDTO;
 import com.codenear.butterfly.global.util.RandomUtil;
 import com.codenear.butterfly.member.application.MemberService;
@@ -22,14 +24,20 @@ public class CertifyService {
     private final MemberService memberService;
 
     public void sendCertifyCode(String phoneNumber) {
-        String randomNum = String.valueOf(RandomUtil.generateRandomNum(CERTIFY_CODE_LENGTH));
-        smsService.sendSMS(phoneNumber, randomNum); // todo : 문자 내용 받으면, 수정해야함
-        redisTemplate.opsForValue().set(phoneNumber, randomNum, CERTIFY_CODE_EXPIRE_MINUTES, TimeUnit.MINUTES);
+        String certifyCode = generateCertifyCode();
+        String certifyMessage = SMS.getMessage(certifyCode);
+
+        smsService.sendSMS(phoneNumber, certifyMessage);
+        redisTemplate.opsForValue().set(phoneNumber, certifyCode, CERTIFY_CODE_EXPIRE_MINUTES, TimeUnit.MINUTES);
     }
 
     public void checkCertifyCode(CertifyRequestDTO requestDTO, MemberDTO memberDTO) {
         String storedCode = redisTemplate.opsForValue().get(requestDTO.getPhoneNumber());
         certifyValidator.validateCertifyCode(storedCode, requestDTO.getCertifyCode());
         memberService.updatePhoneNumber(memberDTO.getId(), requestDTO.getPhoneNumber());
+    }
+
+    private String generateCertifyCode() {
+        return String.valueOf(RandomUtil.generateRandomNum(CERTIFY_CODE_LENGTH));
     }
 }
