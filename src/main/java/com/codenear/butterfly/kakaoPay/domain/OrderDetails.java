89434c718abce3e2ae71486e967e2bb7fcad419c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-28 22:20:58 +0900

    feat: 주문 내역 저장 데이터 추가

--- src/main/java/com/codenear/butterfly/kakaoPay/domain/OrderDetails.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/OrderDetails.java
@@ -2,19 +2,26 @@ package com.codenear.butterfly.kakaoPay.domain;
 
 import com.codenear.butterfly.address.domain.Address;
 import com.codenear.butterfly.kakaoPay.domain.dto.OrderType;
+import com.codenear.butterfly.member.domain.Member;
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
+@Getter
 @Setter
 public class OrderDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
     @Enumerated(EnumType.STRING)
     private OrderType orderType;
@@ -29,4 +36,11 @@ public class OrderDetails {
     @JoinColumn(name = "address_id")
     private Address address;
     private LocalDate deliverDate;
+
+    private String productName;
+    private String productImage;
+    private String optionName;
+    private Integer total;
+    private Integer quantity;
+    private String orderStatus;
 }
