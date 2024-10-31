b84ebe5853144bc181659d4dcb34d072538184e9
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-10-31 10:09:10 +0900

    chore: request DTO 패키지 변경

--- src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/request/DeliveryPaymentRequestDTO.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/request/DeliveryPaymentRequestDTO.java
@@ -1,4 +1,4 @@
-package com.codenear.butterfly.kakaoPay.domain.dto;
+package com.codenear.butterfly.kakaoPay.domain.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
