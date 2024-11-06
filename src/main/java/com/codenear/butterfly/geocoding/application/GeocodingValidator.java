2f5a3259c287fcfb23a04189822b531f881d28cf
taek2222 <oot309@daum.net>
2024-11-07 03:05:42 +0900

    refactor(GeocodingValidator): Import 정리 및 검증 추가 (#262)
    - Error Message 를 Static 선언해 코드를 간소화 했습니다.
    
    - 응답 값에 null 인 부분인 Meta 일때도 발견되어 추가했습니다.

--- src/main/java/com/codenear/butterfly/geocoding/application/GeocodingValidator.java
+++ src/main/java/com/codenear/butterfly/geocoding/application/GeocodingValidator.java
@@ -1,9 +1,10 @@
 package com.codenear.butterfly.geocoding.application;
 
+import static com.codenear.butterfly.geocoding.exception.ErrorMessage.RESPONSE_BODY_NULL;
+import static com.codenear.butterfly.geocoding.exception.ErrorMessage.RESPONSE_TOTAL_COUNT_ZERO;
 import static com.codenear.butterfly.global.exception.ErrorCode.SERVER_ERROR;
 
 import com.codenear.butterfly.geocoding.domain.dto.GeocodingResponse;
-import com.codenear.butterfly.geocoding.exception.ErrorMessage;
 import com.codenear.butterfly.geocoding.exception.GeocodingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,8 @@ public class GeocodingValidator {
     }
 
     private void validateResponseBody(ResponseEntity<GeocodingResponse> response) {
-        if (response.getBody() == null) {
-            throw new GeocodingException(SERVER_ERROR, ErrorMessage.RESPONSE_BODY_NULL);
+        if (response.getBody() == null || response.getBody().getMeta() == null) {
+            throw new GeocodingException(SERVER_ERROR, RESPONSE_BODY_NULL);
         }
     }
 
@@ -32,7 +33,7 @@ public class GeocodingValidator {
 
     private void validateResponseTotalCount(ResponseEntity<GeocodingResponse> response) {
         if (response.getBody().getMeta().getTotalCount() == 0) {
-            throw new GeocodingException(SERVER_ERROR, ErrorMessage.RESPONSE_TOTAL_COUNT_ZERO);
+            throw new GeocodingException(SERVER_ERROR, RESPONSE_TOTAL_COUNT_ZERO);
         }
     }
 }
