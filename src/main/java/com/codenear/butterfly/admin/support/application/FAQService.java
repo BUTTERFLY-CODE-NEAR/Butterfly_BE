49b33200586ebce4126b6cad365c3d15278889e2
taek2222 <oot309@daum.net>
2024-11-18 00:45:49 +0900

    feat(FAQService): FAQ 비즈니스 로직 구현 (#278)
    - Admin 페이지에 필요로한 CRUD를 구성했습니다.

---
+++ src/main/java/com/codenear/butterfly/admin/support/application/FAQService.java
@@ -0,0 +1,47 @@
+package com.codenear.butterfly.admin.support.application;
+
+import static com.codenear.butterfly.global.exception.ErrorCode.*;
+
+import com.codenear.butterfly.admin.exception.AdminException;
+import com.codenear.butterfly.admin.support.domain.dto.FAQRequest;
+import com.codenear.butterfly.support.domain.FAQ;
+import com.codenear.butterfly.support.domain.FAQRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class FAQService {
+
+    private final FAQRepository faqRepository;
+
+    public List<FAQ> getFAQList() {
+        return faqRepository.findAll();
+    }
+
+    public FAQ getFAQ(Long id) {
+        return loadFAQ(id);
+    }
+
+    public FAQ createFAQ(FAQRequest request) {
+        FAQ faq = request.toEntity();
+        return faqRepository.save(faq);
+    }
+
+    public void updateFAQ(Long id, FAQRequest request) {
+        FAQ faq = loadFAQ(id);
+        faq.updateFAQ(request);
+    }
+
+    public void deleteFAQ(Long id) {
+        faqRepository.deleteById(id);
+    }
+
+    private FAQ loadFAQ(Long id) {
+        return faqRepository.findById(id)
+                .orElseThrow(() -> new AdminException(SERVER_ERROR, null));
+    }
+}
