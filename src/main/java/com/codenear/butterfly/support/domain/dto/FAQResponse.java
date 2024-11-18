dfb7a3a3b892b945c5fb388a21e45c4552717055
taek2222 <oot309@daum.net>
2024-11-19 00:49:22 +0900

    feat(FAQResponse): DTO Swagger 추가 (#284)

--- src/main/java/com/codenear/butterfly/support/domain/dto/FAQResponse.java
+++ src/main/java/com/codenear/butterfly/support/domain/dto/FAQResponse.java
@@ -1,15 +1,18 @@
 package com.codenear.butterfly.support.domain.dto;
 
 import com.codenear.butterfly.support.domain.FAQ;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(title = "FAQ 리스트 JSON", description = "FAQ 리스트 요청 시 반환되는 응답 JSON 데이터 입니다.")
 public record FAQResponse(
-        Long id,
-        Contents contents
+        @Schema(description = "FAQ 순번") Long id,
+        @Schema(description = "FAQ 내용") Contents contents
 ){
 
+    @Schema(title = "FAQ 내용 JSON", description = "FAQ 내용과 관련된 응답 데이터 입니다.")
     public record Contents(
-            String question,
-            String answer
+            @Schema(description = "Q. 질문") String question,
+            @Schema(description = "A. 답변") String answer
     ) {
     }
 
