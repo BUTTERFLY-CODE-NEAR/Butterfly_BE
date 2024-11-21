79be794aca626e9975a6ca3c557bad3d81724315
taek2222 <oot309@daum.net>
2024-11-22 01:53:11 +0900

    fix(S3Service): 이미지 미삭제 버그 수정 (#290)
    - 기존 URL을 바탕으로 삭제하려고 시도해 오류가 발생하는 것을 파악해 수정했습니다.
    
    - DB에는 FileName 만 저장할 수 있도록 변경했습니다.

--- src/main/java/com/codenear/butterfly/s3/application/S3Service.java
+++ src/main/java/com/codenear/butterfly/s3/application/S3Service.java
@@ -1,51 +1,59 @@
 package com.codenear.butterfly.s3.application;
 
+import static java.util.UUID.randomUUID;
+import static software.amazon.awssdk.services.s3.model.ObjectCannedACL.PUBLIC_READ;
+
 import com.codenear.butterfly.global.exception.ErrorCode;
 import com.codenear.butterfly.s3.domain.S3Directory;
 import com.codenear.butterfly.s3.exception.S3Exception;
-import java.net.URL;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetUrlRequest;
-import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class S3Service {
-    private final S3Client amazonS3Client;
 
     private static final String BUKKIT_NAME = "codenear";
 
+    private final S3Client amazonS3Client;
+
+    @Value("${cloud.storage.url}")
+    private String storageUrl;
+
     public String uploadFile(MultipartFile file, S3Directory directory) {
         try {
-            String fileKey = createFileKey(directory, file.getOriginalFilename());
+            String fileName = generateFileName();
+            String fileKey = generateFileKey(fileName, directory);
             String contentType = file.getContentType();
 
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(BUKKIT_NAME)
                     .key(fileKey)
                     .contentType(contentType)
-                    .acl(ObjectCannedACL.PUBLIC_READ)
+                    .acl(PUBLIC_READ)
                     .build();
 
             RequestBody requestBody = RequestBody.fromInputStream(file.getInputStream(), file.getSize());
             amazonS3Client.putObject(putObjectRequest, requestBody);
 
-            return generateFileUrl(fileKey);
+            return fileName;
         } catch (Exception e) {
             throw new S3Exception(ErrorCode.SERVER_ERROR, null);
         }
     }
 
-    public void deleteFile(String fileKey) {
+    public String generateFileUrl(String fileName, S3Directory directory) {
+        return storageUrl + directory.getValue() + fileName;
+    }
+
+    public void deleteFile(String fileName, S3Directory directory) {
+        String fileKey = generateFileKey(fileName, directory);
         DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
                 .bucket(BUKKIT_NAME)
                 .key(fileKey)
@@ -54,18 +62,13 @@ public class S3Service {
         amazonS3Client.deleteObject(deleteObjectRequest);
     }
 
-    private String generateFileUrl(String fileKey) {
-        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
-                .bucket(BUKKIT_NAME)
-                .key(fileKey)
-                .build();
-
-        URL url = amazonS3Client.utilities().getUrl(getUrlRequest);
-        return url.toString();
+    private String generateFileName() {
+        String uniqueId = randomUUID().toString();
+        long currentTimeMillis = System.currentTimeMillis();
+        return uniqueId + currentTimeMillis;
     }
 
-    private String createFileKey(S3Directory directory, String originalFilename) {
-        String uniqueId = UUID.randomUUID().toString();
-        return directory.getValue() + uniqueId + originalFilename;
+    private String generateFileKey(String fileName, S3Directory directory) {
+        return directory.getValue() + fileName;
     }
 }
