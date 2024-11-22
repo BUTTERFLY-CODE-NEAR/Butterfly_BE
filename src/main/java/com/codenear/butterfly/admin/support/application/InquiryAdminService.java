5032b3a769506566b9ced8e823f9bb339c9fa055
taek2222 <oot309@daum.net>
2024-11-23 01:17:17 +0900

    feat(InquiryAdminService): 질문 목록 리스트 반환 구현 (#298)

--- src/main/java/com/codenear/butterfly/admin/support/application/InquiryAdminService.java
+++ src/main/java/com/codenear/butterfly/admin/support/application/InquiryAdminService.java
@@ -1,9 +1,12 @@
 package com.codenear.butterfly.admin.support.application;
 
+import com.codenear.butterfly.admin.support.domain.dto.InquiresResponse;
 import com.codenear.butterfly.admin.support.domain.dto.InquiryDetailsResponse;
 import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.support.application.InquiryFacade;
 import com.codenear.butterfly.support.domain.Inquiry;
+import com.codenear.butterfly.support.domain.repositroy.InquiryRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,13 @@ import org.springframework.stereotype.Service;
 public class InquiryAdminService {
 
     private final InquiryFacade inquiryFacade;
+    private final InquiryRepository inquiryRepository;
+
+    public InquiresResponse getInquires() {
+        List<Inquiry> inquiries = inquiryRepository.findAll();
+
+        return InquiresResponse.fromEntity(inquiries);
+    }
 
     public InquiryDetailsResponse getInquiryDetails(Long id) {
         Inquiry inquiry = inquiryFacade.getInquiry(id);
