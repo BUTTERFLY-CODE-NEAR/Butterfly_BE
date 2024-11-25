6951afb6c0e7a6ec0e77439b991f6148f6feb30b
taek2222 <oot309@daum.net>
2024-11-25 21:18:52 +0900

    feat(FCM): FCM Entity 정의 (#317)
    - 오래된 토큰은 삭제하기 위해 마지막으로 사용한 날짜를 기록하도록 설계했습니다.

---
+++ src/main/java/com/codenear/butterfly/fcm/domain/FCM.java
@@ -0,0 +1,39 @@
+package com.codenear.butterfly.fcm.domain;
+
+import com.codenear.butterfly.global.domain.BaseEntity;
+import com.codenear.butterfly.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class FCM {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String token;
+
+    private LocalDateTime lastUsedDate;
+}
