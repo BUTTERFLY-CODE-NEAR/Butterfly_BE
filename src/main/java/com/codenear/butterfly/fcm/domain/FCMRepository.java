2935def218570e6c265487e2972da08e09c965ff
taek2222 <oot309@daum.net>
2024-11-26 16:00:53 +0900

    feat(FCMRepository): FCM MemberId 기반 조회 메서드 추가 (#319)

--- src/main/java/com/codenear/butterfly/fcm/domain/FCMRepository.java
+++ src/main/java/com/codenear/butterfly/fcm/domain/FCMRepository.java
@@ -1,8 +1,10 @@
 package com.codenear.butterfly.fcm.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FCMRepository extends JpaRepository<FCM, Long> {
+    List<FCM> findByMemberId(Long memberId);
 }
