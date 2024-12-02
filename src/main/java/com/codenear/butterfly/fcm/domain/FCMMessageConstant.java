d0c15d2a57ea02d6920685aa3d9c0e6f39b4c220
taek2222 <oot309@daum.net>
2024-12-03 00:38:18 +0900

    feat(FCMMessageConstant): 새로운 상품 Message 상수 추가 (#332)

--- src/main/java/com/codenear/butterfly/fcm/domain/FCMMessageConstant.java
+++ src/main/java/com/codenear/butterfly/fcm/domain/FCMMessageConstant.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum FCMMessageConstant {
-    INQUIRY("질문 답변 완료 ‼\uFE0F", "질문한 내용에 답변이 도착했습니다.", "inquiry");
+    INQUIRY("질문 답변 완료 ‼\uFE0F", "질문한 내용에 답변이 도착했습니다.", "inquiry"),
+    NEW_PRODUCT("놓치지 마세요! 신규 상품 입고 \uD83C\uDF81", "나비에 신상품이 추가됐습니다. 지금 확인하고 가장 먼저 만나보세요 \uD83C\uDF1F", "new");
 
     private final String title;
     private final String body;
