1c0fdb295fa4266216699de5345e3d67f47d1f5f
taek2222 <oot309@daum.net>
2024-12-03 00:42:26 +0900

    refactor(FCMMessageConstant): 페이로드 삭제 및 상수 내용 변경 (#332)
    - 긴급 회의로 결정한 페이로드 삭제 및 결정된 내용으로 변경했습니다.

--- src/main/java/com/codenear/butterfly/fcm/domain/FCMMessageConstant.java
+++ src/main/java/com/codenear/butterfly/fcm/domain/FCMMessageConstant.java
@@ -7,10 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum FCMMessageConstant {
-    INQUIRY("질문 답변 완료 ‼\uFE0F", "질문한 내용에 답변이 도착했습니다.", "inquiry"),
-    NEW_PRODUCT("놓치지 마세요! 신규 상품 입고 \uD83C\uDF81", "나비에 신상품이 추가됐습니다. 지금 확인하고 가장 먼저 만나보세요 \uD83C\uDF1F", "new");
+    INQUIRY_ANSWERED("답변 도착 \uD83D\uDCEC", "문의하신 내용에 답변이 도착했습니다. 지금 바로 확인해 보세요. \uD83D\uDCE8"),
+    NEW_PRODUCT("놓치지 마세요! 신규 상품 입고 \uD83C\uDF81", "나비에 신상품이 추가됐습니다. 지금 확인하고 가장 먼저 만나보세요 \uD83C\uDF1F");
 
     private final String title;
     private final String body;
-    private final String key; // 페이로드
 }
