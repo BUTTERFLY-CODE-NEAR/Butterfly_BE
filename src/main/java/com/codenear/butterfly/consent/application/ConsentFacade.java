8c1344978ea1d10336eacf5c301f1fd861462167
taek2222 <oot309@daum.net>
2024-12-04 21:11:21 +0900

    feat(ConsentFacade): Facade 메소드 추가 (#340)

--- src/main/java/com/codenear/butterfly/consent/application/ConsentFacade.java
+++ src/main/java/com/codenear/butterfly/consent/application/ConsentFacade.java
@@ -1,6 +1,8 @@
 package com.codenear.butterfly.consent.application;
 
 import com.codenear.butterfly.consent.domain.Consent;
+import com.codenear.butterfly.consent.domain.ConsentType;
+import com.codenear.butterfly.member.domain.Member;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,9 +11,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ConsentFacade {
 
-    private final ConsentService consentService;
+    private final ConsentDataAccess consentDataAccess;
 
-    public List<Consent> getConsentByMemberId(Long id) {
-        return consentService.loadConsentsByMemberId(id);
+    public List<Consent> getConsents(Long memberId) {
+        return consentDataAccess.findConsents(memberId);
+    }
+
+    public void saveConsent(ConsentType consentType, boolean agreed, Member member) {
+        Consent consent = Consent.create(consentType, agreed, member);
+        consentDataAccess.save(consent);
     }
 }
