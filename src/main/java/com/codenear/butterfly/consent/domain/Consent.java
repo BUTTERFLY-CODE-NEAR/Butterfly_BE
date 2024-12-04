9f08fd6fa518371b2f99db1e546fad38b25a2f9c
taek2222 <oot309@daum.net>
2024-12-04 21:07:02 +0900

    feat(Consent): 정적 팩토리 메서드 정의 (#340)

--- src/main/java/com/codenear/butterfly/consent/domain/Consent.java
+++ src/main/java/com/codenear/butterfly/consent/domain/Consent.java
@@ -2,8 +2,20 @@ package com.codenear.butterfly.consent.domain;
 
 import com.codenear.butterfly.global.domain.BaseEntity;
 import com.codenear.butterfly.member.domain.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
@@ -23,6 +35,18 @@ public class Consent extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public static Consent create(ConsentType consentType, boolean agreed, Member member) {
+        return Consent.builder()
+                .consentType(consentType)
+                .isAgreed(agreed)
+                .member(member)
+                .build();
+    }
+
+    public static Consent create(ConsentType consentType, Member member) {
+        return create(consentType, false, member);
+    }
 
     public void toggleAgreement() {
         this.isAgreed = !this.isAgreed;
