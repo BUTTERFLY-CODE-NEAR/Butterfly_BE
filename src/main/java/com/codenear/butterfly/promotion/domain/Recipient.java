e034b2f639891ae70c7f0a4100f0c0aa29572ee6
taek2222 <oot309@daum.net>
2024-12-08 00:24:15 +0900

    feat(Recipient): 등록 일자 추가 (#354)

--- src/main/java/com/codenear/butterfly/promotion/domain/Recipient.java
+++ src/main/java/com/codenear/butterfly/promotion/domain/Recipient.java
@@ -1,19 +1,25 @@
 package com.codenear.butterfly.promotion.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Recipient {
 
     @Id
@@ -25,4 +31,8 @@ public class Recipient {
 
     @NotNull
     private String phoneNumber;
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
