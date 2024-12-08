ddb9801061db817511fdf95ebd5e08c16404ea4f
taek2222 <oot309@daum.net>
2024-12-08 23:06:42 +0900

    feat(MemberDataAccess): member 찾기 및 저장 메서드 추가 (#360)
    - Redis 관리에 용이하도록 캐시를 적용했습니다.

--- src/main/java/com/codenear/butterfly/member/infrastructure/MemberDataAccess.java
+++ src/main/java/com/codenear/butterfly/member/infrastructure/MemberDataAccess.java
@@ -1,18 +1,36 @@
 package com.codenear.butterfly.member.infrastructure;
 
+import static com.codenear.butterfly.global.exception.ErrorCode.SERVER_ERROR;
+
 import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.member.domain.repository.member.MemberRepository;
+import com.codenear.butterfly.member.exception.MemberException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class MemberDataAccess {
 
     private final MemberRepository memberRepository;
 
     public Optional<Member> findByPhoneNumber(String phoneNumber) {
         return memberRepository.findByPhoneNumber(phoneNumber);
+    }
+
+    @Cacheable(value = "memberCache", key = "#memberId")
+    public Member findByMemberId(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(SERVER_ERROR, null));
+    }
+
+    @CacheEvict(value = "userCache, memberCache", key = "#member.id")
+    public void save(Member member) {
+        memberRepository.save(member);
     }
 }
