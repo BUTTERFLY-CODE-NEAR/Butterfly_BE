37ba458e5ce03649aa66d43a4da4c3c1fdf8bb1f
taek2222 <oot309@daum.net>
2024-12-08 17:13:37 +0900

    test(PointPromotionTest): 사용 안하는 Import 정리 (#357)

--- src/test/java/com/codenear/butterfly/promotion/domain/PointPromotionTest.java
+++ src/test/java/com/codenear/butterfly/promotion/domain/PointPromotionTest.java
@@ -2,7 +2,6 @@ package com.codenear.butterfly.promotion.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.math.BigDecimal;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
