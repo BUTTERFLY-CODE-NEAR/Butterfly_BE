1067f49dd1d6e798d72c6d04ad4310aaa12141b6
taek2222 <oot309@daum.net>
2024-12-10 00:43:06 +0900

    feat(PhoneRegistrationService): 프로모션 기능 연결 완료 (#360)

--- src/main/java/com/codenear/butterfly/member/application/PhoneRegistrationService.java
+++ src/main/java/com/codenear/butterfly/member/application/PhoneRegistrationService.java
@@ -4,10 +4,12 @@ import static com.codenear.butterfly.certify.domain.CertifyType.REGISTER_PHONE;
 import static com.codenear.butterfly.global.exception.ErrorCode.PHONE_NUMBER_ALREADY_USE;
 
 import com.codenear.butterfly.certify.application.CertifyService;
+import com.codenear.butterfly.certify.domain.dto.CertifyRequest;
 import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
 import com.codenear.butterfly.member.exception.MemberException;
 import com.codenear.butterfly.member.infrastructure.MemberDataAccess;
+import com.codenear.butterfly.promotion.application.CitationPromotionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,15 +20,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class PhoneRegistrationService {
 
     private final CertifyService certifyService;
+    private final CitationPromotionService citationPromotionService;
     private final MemberDataAccess memberDataAccess;
 
-    public void sendRegistrationCode(String phoneNumber, MemberDTO loginMember) {
+    public void sendRegistrationCode(String phoneNumber) {
         validatePhoneNumberDuplicate(phoneNumber);
-
         certifyService.sendCertifyCode(phoneNumber, REGISTER_PHONE);
+    }
 
+    public void checkRegistrationCode(CertifyRequest request, MemberDTO loginMember) {
+        certifyService.checkCertifyCode(request, REGISTER_PHONE);
         Member member = getMemberById(loginMember.getId());
-        updatePhoneNumber(phoneNumber, member);
+        updatePhoneNumber(request.phoneNumber(), member);
+        citationPromotionService.processPromotion(member);
     }
 
     private Member getMemberById(Long memberId) {
