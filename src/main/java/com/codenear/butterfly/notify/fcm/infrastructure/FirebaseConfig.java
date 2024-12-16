de62ab0f79f7228163b05c00c20a4c1a89dae4ac
taek2222 <oot309@daum.net>
2024-12-17 01:17:13 +0900

    chore(FirebaseConfig): Config 관련 패키지 이동

--- src/main/java/com/codenear/butterfly/notify/fcm/infrastructure/FirebaseConfig.java
+++ src/main/java/com/codenear/butterfly/notify/fcm/infrastructure/FirebaseConfig.java
@@ -1,4 +1,4 @@
-package com.codenear.butterfly.notify.fcm.config;
+package com.codenear.butterfly.notify.fcm.infrastructure;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
