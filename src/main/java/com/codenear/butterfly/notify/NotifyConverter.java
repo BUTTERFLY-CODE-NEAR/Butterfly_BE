2a8aecf0024e25d2ad2b29e5e4c08526660be0f6
taek2222 <oot309@daum.net>
2024-12-17 20:32:38 +0900

    feat(NotifyConverter): Notify 컨버터 추가
    - Enum 객체를 JPA 매핑을 위해 추가했습니다.

---
+++ src/main/java/com/codenear/butterfly/notify/NotifyConverter.java
@@ -0,0 +1,18 @@
+package com.codenear.butterfly.notify;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class NotifyConverter implements AttributeConverter<NotifyMessage, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(final NotifyMessage notifyMessage) {
+        return notifyMessage.getCode();
+    }
+
+    @Override
+    public NotifyMessage convertToEntityAttribute(final Integer code) {
+        return NotifyMessage.findByCode(code);
+    }
+}
