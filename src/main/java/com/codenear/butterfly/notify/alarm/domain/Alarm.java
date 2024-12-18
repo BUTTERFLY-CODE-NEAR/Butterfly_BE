238837efd52d2e36a3aa1562568a4d32a2110d37
taek2222 <oot309@daum.net>
2024-12-18 14:20:25 +0900

    feat(Alarm): Builder 패턴 추가 (#377)

--- src/main/java/com/codenear/butterfly/notify/alarm/domain/Alarm.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/domain/Alarm.java
@@ -12,9 +12,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Alarm extends BaseEntity {
 
