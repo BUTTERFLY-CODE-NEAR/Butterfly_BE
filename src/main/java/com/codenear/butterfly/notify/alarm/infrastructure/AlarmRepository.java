ae3cfbc3e32931c1d079f4f317b0138a33f14117
taek2222 <oot309@daum.net>
2024-12-18 14:20:47 +0900

    fix(AlarmRepository): 어노테이션 명시 (#377)

--- src/main/java/com/codenear/butterfly/notify/alarm/infrastructure/AlarmRepository.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/infrastructure/AlarmRepository.java
@@ -3,7 +3,9 @@ package com.codenear.butterfly.notify.alarm.infrastructure;
 import com.codenear.butterfly.notify.alarm.domain.Alarm;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findByMemberId(Long memberId);
 }
