ad0802e1f80e2f19815324e6f476c6c1d2be5fa0
taek2222 <oot309@daum.net>
2024-12-19 00:58:12 +0900

    feat(AlarmService): 알림 목록 추가 기능 구현 (#379)

--- src/main/java/com/codenear/butterfly/notify/alarm/application/AlarmService.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/application/AlarmService.java
@@ -1,5 +1,7 @@
 package com.codenear.butterfly.notify.alarm.application;
 
+import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.notify.NotifyMessage;
 import com.codenear.butterfly.notify.alarm.domain.Alarm;
 import com.codenear.butterfly.notify.alarm.domain.dto.AlarmsResponse;
 import com.codenear.butterfly.notify.alarm.infrastructure.AlarmRepository;
@@ -19,5 +21,18 @@ public class AlarmService {
         List<Alarm> alarms = alarmRepository.findByMemberId(memberId);
         alarmRepository.markAllAsReadByMemberId(memberId); // 전체 읽음 처리
         return AlarmsResponse.of(alarms);
+    }
+
+    public void addAlarm(Member member, NotifyMessage message) {
+        Alarm newAlarm = createAlarm(member, message);
+        alarmRepository.save(newAlarm);
+    }
+
+    private static Alarm createAlarm(final Member member, final NotifyMessage message) {
+        return Alarm.builder()
+                .notifyMessage(message)
+                .member(member)
+                .isNew(true)
+                .build();
     }
 }
