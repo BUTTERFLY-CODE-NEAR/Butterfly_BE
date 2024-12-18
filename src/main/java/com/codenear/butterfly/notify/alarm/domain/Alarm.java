5b0de5f18da47d632eb9f532c8a712e4e2121ac4
taek2222 <oot309@daum.net>
2024-12-18 09:13:40 +0900

    feat(Alarm): BaseEntity 적용 및 Member JPA 연관 매핑

--- src/main/java/com/codenear/butterfly/notify/alarm/domain/Alarm.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/domain/Alarm.java
@@ -1,15 +1,22 @@
 package com.codenear.butterfly.notify.alarm.domain;
 
+import com.codenear.butterfly.global.domain.BaseEntity;
+import com.codenear.butterfly.member.domain.Member;
 import com.codenear.butterfly.notify.NotifyConverter;
 import com.codenear.butterfly.notify.NotifyMessage;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 
 @Entity
-public class Alarm {
+@Getter
+public class Alarm extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +24,10 @@ public class Alarm {
 
     @Convert(converter = NotifyConverter.class)
     private NotifyMessage notifyMessage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     private boolean isNew;
 }
