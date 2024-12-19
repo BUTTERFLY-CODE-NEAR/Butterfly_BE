d3986b87dec198f1242832fdddb6b6b285dde0a9
taek2222 <oot309@daum.net>
2024-12-19 13:20:15 +0900

    feat(AlarmControllerSwagger): 알림 조회 컨트롤러 정의 (#383)
    - 알림 Swagger도 추가로 정의했습니다.

---
+++ src/main/java/com/codenear/butterfly/notify/alarm/presentation/AlarmController.java
@@ -0,0 +1,27 @@
+package com.codenear.butterfly.notify.alarm.presentation;
+
+import com.codenear.butterfly.global.dto.ResponseDTO;
+import com.codenear.butterfly.global.util.ResponseUtil;
+import com.codenear.butterfly.member.domain.dto.MemberDTO;
+import com.codenear.butterfly.notify.alarm.application.AlarmService;
+import com.codenear.butterfly.notify.alarm.domain.dto.AlarmsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notify/alarm")
+public class AlarmController implements AlarmControllerSwagger {
+
+    private final AlarmService alarmService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO> getAlarms(@AuthenticationPrincipal MemberDTO loginMember) {
+        AlarmsResponse alarms = alarmService.getAlarmsByMemberId(loginMember.getId());
+        return ResponseUtil.createSuccessResponse(alarms);
+    }
+}
