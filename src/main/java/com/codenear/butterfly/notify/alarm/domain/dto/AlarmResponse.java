74998d6342810d9d7ac5aad8593642d56444e436
taek2222 <oot309@daum.net>
2024-12-19 13:20:48 +0900

    feat: Response 데이터 Swagger 추가 (#383)

--- src/main/java/com/codenear/butterfly/notify/alarm/domain/dto/AlarmResponse.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/domain/dto/AlarmResponse.java
@@ -2,18 +2,20 @@ package com.codenear.butterfly.notify.alarm.domain.dto;
 
 import com.codenear.butterfly.notify.NotifyMessage;
 import com.codenear.butterfly.notify.alarm.domain.Alarm;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
+@Schema(title = "알림 JSON", description = "알림 요청 시 반환되는 응답 JSON 데이터 입니다.")
 public record AlarmResponse(
-        Long id,
-        Info info,
-        boolean isNew
+        @Schema(description = "알림 ID") Long id,
+        @Schema(description = "알림 정보") Info info,
+        @Schema(description = "새로운 알림 여부") boolean isNew
 ) {
     private record Info(
-            String title,
-            String subtitle,
-            String content,
-            LocalDateTime createdAt
+            @Schema(description = "제목") String title,
+            @Schema(description = "소제목") String subtitle,
+            @Schema(description = "내용") String content,
+            @Schema(description = "시간") LocalDateTime createdAt
     ) {
     }
 
--- src/main/java/com/codenear/butterfly/notify/alarm/domain/dto/AlarmsResponse.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/domain/dto/AlarmsResponse.java
@@ -1,10 +1,12 @@
 package com.codenear.butterfly.notify.alarm.domain.dto;
 
 import com.codenear.butterfly.notify.alarm.domain.Alarm;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
+@Schema(title = "알림 목록 JSON", description = "알림 목록 요청 시 반환되는 응답 JSON 데이터 입니다.")
 public record AlarmsResponse(
-        List<AlarmResponse> alarmResponses
+        @Schema(description = "알림 목록") List<AlarmResponse> alarmResponses
 ) {
     
     public static AlarmsResponse of(final List<Alarm> alarms) {
