483845e2f3c7dd9a56b5725f5570c7e03add2566
taek2222 <oot309@daum.net>
2024-12-19 13:30:48 +0900

    refactor(AlarmResponse): 알림 요청 시 반환되는 응답 데이터 형태 변경 (#383)
    - FE 요청으로, 알림 요청 시 반환되는 응답 데이터를 변경했습니다.

--- src/main/java/com/codenear/butterfly/notify/alarm/domain/dto/AlarmResponse.java
+++ src/main/java/com/codenear/butterfly/notify/alarm/domain/dto/AlarmResponse.java
@@ -8,33 +8,22 @@ import java.time.LocalDateTime;
 @Schema(title = "알림 JSON", description = "알림 요청 시 반환되는 응답 JSON 데이터 입니다.")
 public record AlarmResponse(
         @Schema(description = "알림 ID") Long id,
-        @Schema(description = "알림 정보") Info info,
+        @Schema(description = "제목") String title,
+        @Schema(description = "소제목") String subtitle,
+        @Schema(description = "내용") String content,
+        @Schema(description = "시간") LocalDateTime createdAt,
         @Schema(description = "새로운 알림 여부") boolean isNew
-) {
-    private record Info(
-            @Schema(description = "제목") String title,
-            @Schema(description = "소제목") String subtitle,
-            @Schema(description = "내용") String content,
-            @Schema(description = "시간") LocalDateTime createdAt
     ) {
-    }
 
     public static AlarmResponse of(final Alarm alarm) {
-        Info info = createInfo(alarm);
+        NotifyMessage message = alarm.getNotifyMessage();
         return new AlarmResponse(
                 alarm.getId(),
-                info,
-                alarm.isNew()
-        );
-    }
-
-    private static Info createInfo(final Alarm alarm) {
-        NotifyMessage message = alarm.getNotifyMessage();
-        return new Info(
                 message.getTitle(),
                 message.getSubtitle(),
                 message.getContent(),
-                alarm.getCreatedAt()
+                alarm.getCreatedAt(),
+                alarm.isNew()
         );
     }
 }
