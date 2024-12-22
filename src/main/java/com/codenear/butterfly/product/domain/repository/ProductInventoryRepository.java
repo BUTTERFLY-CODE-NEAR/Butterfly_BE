2c88e0f3968bde2781b240ca8de06a8fc43700f1
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-12-23 00:59:47 +0900

    feat(ProductInventoryRepository): 상품 동적 데이터 repository 추가

---
+++ src/main/java/com/codenear/butterfly/product/domain/repository/ProductInventoryRepository.java
@@ -0,0 +1,16 @@
+package com.codenear.butterfly.product.domain.repository;
+
+import com.codenear.butterfly.product.domain.Category;
+import com.codenear.butterfly.product.domain.ProductInventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductInventoryRepository extends JpaRepository<ProductInventory, Long> {
+
+    List<ProductInventory> findProductByCategory(Category category);
+
+    ProductInventory findProductByProductName(String productName);
+}
