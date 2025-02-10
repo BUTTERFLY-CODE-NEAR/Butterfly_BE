2d1b7fce9e875264f8396e92ef8b2c77e26b0282
jinu-ahn <dkswlsdn98@naver.com>
2025-02-10 16:31:43 +0900

    feat(descriptionImage): 상품 설명 이미지 테이블 생성

---
+++ src/main/java/com/codenear/butterfly/product/domain/ProductDescriptionImage.java
@@ -0,0 +1,34 @@
+package com.codenear.butterfly.product.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ProductDescriptionImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Builder
+    public ProductDescriptionImage(String imgUrl, Product product) {
+        this.imageUrl = imgUrl;
+        this.product = product;
+    }
+}
---
+++ src/main/java/com/codenear/butterfly/product/domain/repository/ProductDescriptionImageRepository.java
@@ -0,0 +1,7 @@
+package com.codenear.butterfly.product.domain.repository;
+
+import com.codenear.butterfly.product.domain.ProductDescriptionImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductDescriptionImageRepository extends JpaRepository<ProductDescriptionImage, Long> {
+}
