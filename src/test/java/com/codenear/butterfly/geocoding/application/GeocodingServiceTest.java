c4a34b8a9aa0b357681e44e3b2c8896d9d4f6e05
jinu-ahn <dkswlsdn98@naver.com>
2025-02-12 01:03:45 +0900

    test: activeProfiles 변경

--- src/test/java/com/codenear/butterfly/geocoding/application/GeocodingServiceTest.java
+++ src/test/java/com/codenear/butterfly/geocoding/application/GeocodingServiceTest.java
@@ -1,14 +1,14 @@
 package com.codenear.butterfly.geocoding.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 class GeocodingServiceTest {
 
     @Autowired
