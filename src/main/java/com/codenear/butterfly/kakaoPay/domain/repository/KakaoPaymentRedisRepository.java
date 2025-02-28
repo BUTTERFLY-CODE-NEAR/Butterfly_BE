fe90df52cd81bfd8354662f80b3d42e76614716b
jinu-ahn <dkswlsdn98@naver.com>
2025-02-28 18:41:14 +0900

    fix(KakaoPaymentRedisRepository): TTL 시간 적용 안되는 오류 수정

--- src/main/java/com/codenear/butterfly/kakaoPay/domain/repository/KakaoPaymentRedisRepository.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/repository/KakaoPaymentRedisRepository.java
@@ -168,10 +168,7 @@ public class KakaoPaymentRedisRepository {
      * TTL 시간 설정
      */
     private void ensureTTL(final String key) {
-        // Hash Key가 처음 생성된 경우에만 TTL 설정
-        if (Boolean.FALSE.equals(redisTemplate.hasKey(key))) {
-            // TTL 15분 설정 (kakao pay API가 호출 후 생성되는 tid의 유효기간은 15분 이기에 15분으로 설정)
-            redisTemplate.expire(key, TIME_TO_LIVE_MINUTE, TimeUnit.MINUTES);
-        }
+        // TTL 15분 설정 (kakao pay API가 호출 후 생성되는 tid의 유효기간은 15분 이기에 15분으로 설정)
+        redisTemplate.expire(key, TIME_TO_LIVE_MINUTE, TimeUnit.MINUTES);
     }
 }
