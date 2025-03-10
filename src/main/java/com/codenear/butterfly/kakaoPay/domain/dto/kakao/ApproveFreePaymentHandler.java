78c39a06087867fe3ce69226f15951b7fdae843f
jinu-ahn <dkswlsdn98@naver.com>
2025-03-11 03:24:13 +0900

    feat(ApproveHandler): 주문 가격에 따른 필요 정보 handling

---
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/dto/kakao/ApproveFreePaymentHandler.java
@@ -0,0 +1,44 @@
+package com.codenear.butterfly.kakaoPay.domain.dto.kakao;
+
+import com.codenear.butterfly.kakaoPay.domain.Amount;
+import com.codenear.butterfly.kakaoPay.domain.SinglePayment;
+import com.codenear.butterfly.kakaoPay.domain.dto.request.BasePaymentRequestDTO;
+import lombok.Getter;
+
+@Getter
+public class ApproveFreePaymentHandler extends ApproveHandler {
+    private final BasePaymentRequestDTO basePaymentRequestDTO;
+    private final Long memberId;
+
+    public ApproveFreePaymentHandler(BasePaymentRequestDTO basePaymentRequestDTO, String orderId, Long memberId) {
+        super(
+                orderId,
+                basePaymentRequestDTO.getProductName(),
+                basePaymentRequestDTO.getQuantity(),
+                basePaymentRequestDTO.getPoint()
+        );
+        this.basePaymentRequestDTO = basePaymentRequestDTO;
+        this.memberId = memberId;
+    }
+
+    @Override
+    public SinglePayment createSinglePayment() {
+        return SinglePayment.freeOrderBuilder()
+                .partnerOrderId(orderId)
+                .partnerUserId(memberId)
+                .basePaymentRequestDTO(basePaymentRequestDTO)
+                .buildFreeOrder();
+    }
+
+    @Override
+    public Amount createAmount() {
+        return Amount.freeOrderBuilder()
+                .basePaymentRequestDTO(basePaymentRequestDTO)
+                .buildFreeOrder();
+    }
+
+    @Override
+    public Object getOrderDetailDto() {
+        return basePaymentRequestDTO;
+    }
+}
