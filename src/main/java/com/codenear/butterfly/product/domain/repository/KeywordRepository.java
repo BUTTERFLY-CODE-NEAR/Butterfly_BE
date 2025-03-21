eb9a3bcb17c0b180b50693668ac487b5a7679d84
jinu-ahn <dkswlsdn98@naver.com>
2025-03-21 16:33:53 +0900

    feat(SearchService): 키워드 검색

--- src/main/java/com/codenear/butterfly/product/domain/repository/KeywordRepository.java
+++ src/main/java/com/codenear/butterfly/product/domain/repository/KeywordRepository.java
@@ -2,11 +2,17 @@ package com.codenear.butterfly.product.domain.repository;
 
 import com.codenear.butterfly.product.domain.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     Optional<Keyword> findKeywordByKeyword(String keyword);
+
+    @Query(value = "SELECT product_id FROM keyword WHERE keyword = :keyword", nativeQuery = true)
+    List<Long> findAllProductIdByKeyword(@Param("keyword") String keyword);
 }
--- src/main/java/com/codenear/butterfly/search/application/SearchService.java
+++ src/main/java/com/codenear/butterfly/search/application/SearchService.java
@@ -1,11 +1,20 @@
 package com.codenear.butterfly.search.application;
 
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
+import com.codenear.butterfly.product.domain.ProductInventory;
+import com.codenear.butterfly.product.domain.dto.ProductViewDTO;
+import com.codenear.butterfly.product.domain.repository.FavoriteRepository;
+import com.codenear.butterfly.product.domain.repository.KeywordRepository;
+import com.codenear.butterfly.product.domain.repository.ProductInventoryRepository;
+import com.codenear.butterfly.product.util.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,6 +27,13 @@ public class SearchService {
     public static final int SEARCH_LOG_MAX_SIZE = 5;
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final FavoriteRepository favoriteRepository;
+    private final ProductInventoryRepository productInventoryRepository;
+    private final KeywordRepository keywordRepository;
+
+    private static String getKey(MemberDTO memberDTO) {
+        return SEARCH_LOG_KEY_PREFIX + memberDTO.getId();
+    }
 
     public List<String> getRelatedKeywords(String keyword) {
         Set<Object> keywords = redisTemplate.opsForSet().members(KEYWORDS_PREFIX);
@@ -31,18 +47,34 @@ public class SearchService {
                 .collect(Collectors.toList());
     }
 
-    public void addSearchLog(String keyword, MemberDTO memberDTO) { // todo : 추후 검색 로직에 추가해 로그 남기는 메서드 전달
-        String key = getKey(memberDTO);
+    /**
+     * keyword에 포함되는 상품을 검색하고, 사용자 검색로그에 저장한다.
+     *
+     * @param keyword   검색어
+     * @param memberDTO 사용자
+     * @return 상품 리스트
+     */
+    @Transactional(readOnly = true)
+    public List<ProductViewDTO> search(String keyword, MemberDTO memberDTO) {
+        List<String> relatedKeywords = getRelatedKeywords(keyword);
+        List<Long> favoriteProductIds = favoriteRepository.findAllProductIdByMemberId(memberDTO.getId());
+        Set<Long> favoriteProductIdSet = new HashSet<>(favoriteProductIds);
 
-        if (redisTemplate.opsForList().indexOf(key, keyword) != null)
-            redisTemplate.opsForList().remove(key, 0, keyword);
+        List<ProductViewDTO> searchProduct = relatedKeywords.stream()
+                .flatMap(relateKeyword -> {
+                    List<ProductInventory> products = findKeywordByProductList(relateKeyword);
 
-        redisTemplate.opsForList().leftPush(key, keyword);
+                    return products.stream()
+                            .sorted((p1, p2) -> Boolean.compare(p1.isSoldOut(), p2.isSoldOut()))
+                            .map(product -> ProductMapper.toProductViewDTO(product,
+                                    favoriteProductIdSet.contains(product.getId()),
+                                    product.calculateGauge()));
+                })
+                .distinct()
+                .toList();
 
-        Long size = redisTemplate.opsForList().size(key);
-
-        if (size != null && size > SEARCH_LOG_MAX_SIZE)
-            redisTemplate.opsForList().rightPop(key);
+        addSearchLog(keyword, memberDTO);
+        return searchProduct;
     }
 
     public List<Object> getSearchList(MemberDTO memberDTO) {
@@ -58,7 +90,27 @@ public class SearchService {
         redisTemplate.opsForList().remove(key, 0, keyword);
     }
 
-    private static String getKey(MemberDTO memberDTO) {
-        return SEARCH_LOG_KEY_PREFIX + memberDTO.getId();
+    private void addSearchLog(String keyword, MemberDTO memberDTO) {
+        String key = getKey(memberDTO);
+
+        if (redisTemplate.opsForList().indexOf(key, keyword) != null)
+            redisTemplate.opsForList().remove(key, 0, keyword);
+
+        redisTemplate.opsForList().leftPush(key, keyword);
+
+        Long size = redisTemplate.opsForList().size(key);
+
+        if (size != null && size > SEARCH_LOG_MAX_SIZE)
+            redisTemplate.opsForList().rightPop(key);
+    }
+
+    private List<ProductInventory> findKeywordByProductList(String keyword) {
+        return keywordRepository.findAllProductIdByKeyword(keyword)
+                .stream()
+                .map(productInventoryRepository::findById)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .distinct()
+                .toList();
     }
 }
