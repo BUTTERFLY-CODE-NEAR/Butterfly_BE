4395df5d9a9b4e83f4d2a2ccdf14aa82612ca06d
jinu-ahn <dkswlsdn98@naver.com>
2025-03-26 17:35:24 +0900

    refactor: keyword를 위한 Redis repository 생성

--- src/main/java/com/codenear/butterfly/product/application/KeywordService.java
+++ src/main/java/com/codenear/butterfly/product/application/KeywordService.java
@@ -1,10 +1,10 @@
 package com.codenear.butterfly.product.application;
 
 import com.codenear.butterfly.product.domain.Keyword;
+import com.codenear.butterfly.product.domain.repository.KeywordRedisRepository;
 import com.codenear.butterfly.product.domain.repository.KeywordRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,19 +12,15 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class KeywordService {
-    public static final String KEYWORDS_PREFIX = "keywords";
-
     private final KeywordRepository keywordRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final KeywordRedisRepository keywordRedisRepository;
 
     @PostConstruct
     public void loadKeywordsIntoRedis() {
         List<Keyword> keywords = keywordRepository.findAll();
 
-        redisTemplate.delete(KEYWORDS_PREFIX);
+        keywordRedisRepository.deleteKeyword();
+        keywordRedisRepository.saveKeyword(keywords);
 
-        for (Keyword keyword : keywords) {
-            redisTemplate.opsForSet().add(KEYWORDS_PREFIX, keyword.getKeyword());
-        }
     }
 }
