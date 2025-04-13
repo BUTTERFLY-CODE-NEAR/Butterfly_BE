5c2a6dac41a05bf0c36db2546724415291b3dc48
Junho Choi <tig9659@ajou.ac.kr>
2025-04-13 10:30:57 +0900

    Refactor :  일괄배송 배치처리

--- src/main/java/com/codenear/butterfly/kakaoPay/domain/repository/OrderDetailsRepository.java
+++ src/main/java/com/codenear/butterfly/kakaoPay/domain/repository/OrderDetailsRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
@@ -14,4 +18,10 @@ public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long
     OrderDetails findByOrderCode(String orderCode);
 
     Page<OrderDetails> findByOrderStatus(OrderStatus status, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE OrderDetails o SET o.orderStatus = :newStatus WHERE o.id IN :orderIds AND o.orderStatus = :currentStatus")
+    int updateOrderStatusInBulk(@Param("orderIds") List<Long> orderIds,
+                                @Param("currentStatus") OrderStatus currentStatus,
+                                @Param("newStatus") OrderStatus newStatus);
 }
--- src/main/java/com/codenear/butterfly/point/domain/PointRepository.java
+++ src/main/java/com/codenear/butterfly/point/domain/PointRepository.java
@@ -1,12 +1,22 @@
 package com.codenear.butterfly.point.domain;
 
 import com.codenear.butterfly.member.domain.Member;
+import com.codenear.butterfly.product.domain.ProductInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
     Optional<Point> findByMember(Member member);
+
+    @Modifying
+    @Query("UPDATE Point p SET p.point = p.point + :amount WHERE p.member.id = :memberId")
+    void increasePointByMemberId(@Param("memberId") Long memberId, @Param("amount") int amount);
 }
