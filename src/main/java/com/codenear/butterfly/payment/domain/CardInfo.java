7a288364e8448c227d992e3a8db6a7b7661f51c6
jinu-ahn <dkswlsdn98@naver.com>
2025-04-28 20:36:48 +0900

    refactor(CardInfo): 필드 명 변경 (kakaopay -> common)

--- src/main/java/com/codenear/butterfly/payment/domain/CardInfo.java
+++ src/main/java/com/codenear/butterfly/payment/domain/CardInfo.java
@@ -19,10 +19,10 @@ public class CardInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String kakaopayPurchaseCorp; // 카카오페이 매입사명
-    private String kakaopayPurchaseCorpCode; // 카카오페이 매입사 코드
-    private String kakaopayIssuerCorp; // 카카오페이 발급사명
-    private String kakaopayIssuerCorpCode; // 카카오페이 발급사 코드
+    private String purchaseCorp; // 매입사명
+    private String purchaseCorpCode; // 매입사 코드
+    private String issuerCorp; // 발급사명
+    private String issuerCorpCode; // 발급사 코드
     private String bin; // 카드 BIN
     private String cardType; // 카드 타입
     private String installMonth; // 할부 개월 수
@@ -45,10 +45,10 @@ public class CardInfo {
         this.cardItemCode = approveResponseDTO.getCard_info().getCard_item_code();
         this.installmentType = approveResponseDTO.getCard_info().getInstallment_type();
         this.interestFreeInstall = approveResponseDTO.getCard_info().getInterest_free_install();
-        this.kakaopayPurchaseCorp = approveResponseDTO.getCard_info().getKakaopay_purchase_corp();
-        this.kakaopayPurchaseCorpCode = approveResponseDTO.getCard_info().getKakaopay_purchase_corp_code();
-        this.kakaopayIssuerCorp = approveResponseDTO.getCard_info().getKakaopay_issuer_corp();
-        this.kakaopayIssuerCorpCode = approveResponseDTO.getCard_info().getKakaopay_issuer_corp_code();
+        this.purchaseCorp = approveResponseDTO.getCard_info().getKakaopay_purchase_corp();
+        this.purchaseCorpCode = approveResponseDTO.getCard_info().getKakaopay_purchase_corp_code();
+        this.issuerCorp = approveResponseDTO.getCard_info().getKakaopay_issuer_corp();
+        this.issuerCorpCode = approveResponseDTO.getCard_info().getKakaopay_issuer_corp_code();
     }
 
     @Builder(builderMethodName = "tossPaymentBuilder", buildMethodName = "buildTossPayment")
@@ -60,7 +60,7 @@ public class CardInfo {
         this.cardType = card.getCardType();
         this.installMonth = String.valueOf(card.getInstallmentPlanMonths());
         this.interestFreeInstall = card.getInstallmentPlanMonths() == 0 ? "Y" : "N";
-        this.kakaopayIssuerCorpCode = card.getIssuerCode();
-        this.kakaopayPurchaseCorpCode = card.getAcquirerCode();
+        this.issuerCorpCode = card.getIssuerCode();
+        this.purchaseCorpCode = card.getAcquirerCode();
     }
 }
