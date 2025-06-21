fb28a1a1530491919cb5f18c7893a5892067f449
jinu-ahn <dkswlsdn98@naver.com>
2025-06-21 14:38:21 +0900

    refactor(toss): response에 orderId 추가

--- src/main/java/com/codenear/butterfly/payment/tossPay/application/TossPaymentService.java
+++ src/main/java/com/codenear/butterfly/payment/tossPay/application/TossPaymentService.java
@@ -1,12 +1,13 @@
 package com.codenear.butterfly.payment.tossPay.application;
 
 import com.codenear.butterfly.payment.domain.dto.request.BasePaymentRequestDTO;
+import com.codenear.butterfly.payment.tossPay.domain.dto.ReadyResponseDTO;
 import com.codenear.butterfly.payment.tossPay.domain.dto.TossPaymentCancelRequestDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface TossPaymentService {
-    void paymentReady(BasePaymentRequestDTO basePaymentRequestDTO, Long memberId, String orderType);
+    ReadyResponseDTO paymentReady(BasePaymentRequestDTO basePaymentRequestDTO, Long memberId, String orderType);
 
     void confirm(Long memberId, String paymentKey, String orderId, int amount);
 
