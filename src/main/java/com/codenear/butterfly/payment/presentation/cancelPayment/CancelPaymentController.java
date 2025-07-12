1cdcee175c3f5be022b293f5582ae32a36f09b20
jinu-ahn <dkswlsdn98@naver.com>
2025-07-13 00:23:30 +0900

    refactor(CancelPayment): 패키지 변경

--- src/main/java/com/codenear/butterfly/payment/presentation/cancelPayment/CancelPaymentController.java
+++ src/main/java/com/codenear/butterfly/payment/presentation/cancelPayment/CancelPaymentController.java
@@ -1,10 +1,10 @@
-package com.codenear.butterfly.payment.kakaoPay.presentation.cancelPayment;
+package com.codenear.butterfly.payment.presentation.cancelPayment;
 
 import com.codenear.butterfly.global.dto.ResponseDTO;
 import com.codenear.butterfly.global.util.ResponseUtil;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
+import com.codenear.butterfly.payment.application.PaymentCancelService;
 import com.codenear.butterfly.payment.domain.dto.request.CancelRequestDTO;
-import com.codenear.butterfly.payment.kakaoPay.application.CancelPaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/payment/kakaopay-cancel")
+@RequestMapping("/payment/cancel")
 @RequiredArgsConstructor
 public class CancelPaymentController implements CancelPaymentControllerSwagger {
 
-    private final CancelPaymentService cancelPaymentService;
+    private final PaymentCancelService paymentCancelService;
 
     @PostMapping
     public ResponseEntity<ResponseDTO> cancelPaymentRequest(@RequestBody CancelRequestDTO cancelRequestDTO,
                                                             @AuthenticationPrincipal MemberDTO memberDTO) {
-        cancelPaymentService.cancelKakaoPay(cancelRequestDTO);
+        paymentCancelService.cancelPayment(cancelRequestDTO);
         return ResponseUtil.createSuccessResponse(HttpStatus.OK, "결제 취소가 성공적으로 완료되었습니다.", null);
     }
 }
--- src/main/java/com/codenear/butterfly/payment/presentation/cancelPayment/CancelPaymentControllerSwagger.java
+++ src/main/java/com/codenear/butterfly/payment/presentation/cancelPayment/CancelPaymentControllerSwagger.java
@@ -1,4 +1,4 @@
-package com.codenear.butterfly.payment.kakaoPay.presentation.cancelPayment;
+package com.codenear.butterfly.payment.presentation.cancelPayment;
 
 import com.codenear.butterfly.global.dto.ResponseDTO;
 import com.codenear.butterfly.member.domain.dto.MemberDTO;
