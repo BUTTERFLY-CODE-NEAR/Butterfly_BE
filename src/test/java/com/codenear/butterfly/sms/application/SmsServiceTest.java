bfbc6caec398243e6163af6469071c1c825f7efd
jinu-ahn <dkswlsdn98@naver.com>
2025-07-15 15:25:14 +0900

    test(SmsServiceTest): coolsms 테스트에서 ncloudSms로 변경

--- src/test/java/com/codenear/butterfly/sms/application/SmsServiceTest.java
+++ src/test/java/com/codenear/butterfly/sms/application/SmsServiceTest.java
@@ -1,6 +1,6 @@
 package com.codenear.butterfly.sms.application;
 
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import com.codenear.butterfly.sms.domain.dto.CloudSmsResponseDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,19 +14,20 @@ class SmsServiceTest {
     @Autowired
     private SmsService smsService;
 
-    @Value("${cool.sms.test}")
+    @Value("${cloud-sms.test}")
     private String fromPhoneNumber;
 
     @Test
-    @Disabled // 포인트 차감으로, 최종 테스트만 사용 권장
+    @Disabled
+        // 포인트 차감으로, 최종 테스트만 사용 권장
     void 문자_전송_테스트() {
         // given
         String testMessage = "Test Message";
 
         // when
-        SingleMessageSentResponse response = smsService.sendSMS(fromPhoneNumber, testMessage);
+        CloudSmsResponseDTO response = smsService.sendSMS(fromPhoneNumber, testMessage);
 
         // then
-        Assertions.assertEquals(response.getStatusCode(), "2000"); // coolSms 성공 상태 코드가 "2000"
+        Assertions.assertEquals(response.statusName(), "success");
     }
 }
