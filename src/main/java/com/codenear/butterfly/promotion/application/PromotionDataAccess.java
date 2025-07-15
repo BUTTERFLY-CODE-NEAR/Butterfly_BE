05763d36a4e3e678254f27acda5a3ee8f25f6f28
jinu-ahn <dkswlsdn98@naver.com>
2025-07-15 14:54:00 +0900

    refactor(PromotionDataAccess): promotion 캐싱 제거

--- src/main/java/com/codenear/butterfly/promotion/application/PromotionDataAccess.java
+++ src/main/java/com/codenear/butterfly/promotion/application/PromotionDataAccess.java
@@ -1,13 +1,12 @@
 package com.codenear.butterfly.promotion.application;
 
-import static com.codenear.butterfly.global.exception.ErrorCode.SERVER_ERROR;
-
 import com.codenear.butterfly.promotion.domain.PointPromotion;
 import com.codenear.butterfly.promotion.domain.repository.PointPromotionRepository;
 import com.codenear.butterfly.promotion.exception.PromotionException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
+import static com.codenear.butterfly.global.exception.ErrorCode.SERVER_ERROR;
 
 @Component
 @RequiredArgsConstructor
@@ -15,10 +14,7 @@ public class PromotionDataAccess {
 
     private final PointPromotionRepository pointPromotionRepository;
 
-//    @Cacheable(value = "pointPromotion", key = "#promotionId")
-    @Cacheable(value = "pointPromotion", key = "#promotion?.id", condition = "#result != null")
     public PointPromotion findPointPromotion(Long promotionId) {
-        System.out.println("데이터베이스에서 조회: " + promotionId);
         return pointPromotionRepository.findById(promotionId)
                 .orElseThrow(() -> new PromotionException(SERVER_ERROR, null));
     }
